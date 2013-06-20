07a13c0caa1104d5fe48d371ed4e1a0078d05ab4
jpata <joosep.pata@gmail.com>
2013-06-20 17:31:05 +0300

    added argument parsing

--- qcd_estimation/get_qcd_yield.py
+++ qcd_estimation/get_qcd_yield.py
@@ -13,11 +13,6 @@ from util_scripts import *
 from DataLumiStorage import *
 from plots.common.cross_sections import lumi_iso, lumi_antiiso
 
-try:
-    sys.path.append(os.environ["STPOL_DIR"] )
-except KeyError:
-    print "Could not find the STPOL_DIR environment variable, did you run `source setenv.sh` in the code base directory?"
-    sys.exit(1)
 
 def get_yield(var, filename, cutMT, mtMinValue, fit_result, dataGroup):
     infile = "fits/"+var.shortName+"_fit_"+filename+".root"
@@ -47,7 +42,7 @@ def get_qcd_yield(var, cuts, cutMT, mtMinValue, dataGroup, lumis, MCGroups, syst
     return y
 
 def get_qcd_yield_with_fit(var, cuts, cutMT, mtMinValue, dataGroup, lumis, MCGroups, systematics, openedFiles, useMCforQCDTemplate, QCDGroup):
-    fit = Fit()    
+    fit = Fit()
     make_histos_with_cuts(var, cuts, dataGroup, MCGroups, systematics, lumis, openedFiles, fit, useMCforQCDTemplate, QCDGroup)
     fit_qcd(var, cuts.name, fit)
     dataHisto = dataGroup.getHistogram(var,  "Nominal", "iso", cuts.name)
@@ -56,23 +51,23 @@ def get_qcd_yield_with_fit(var, cuts, cutMT, mtMinValue, dataGroup, lumis, MCGro
     return (get_yield(var, cuts.name, cutMT, mtMinValue, fit, dataGroup), fit)
 
 
-def get_qcd_yield_with_selection(cuts, channel = "mu"):
-    do_systematics = True
-
-    if channel == "ele":
-        do_systematics = False
-
+def get_qcd_yield_with_selection(cuts, channel = "mu", base_path="$STPOL_DIR/step3_latest/", do_systematics=False):
+#    do_systematics = True
+#
+#    if channel == "ele":
+#        do_systematics = False
+#
     print "QCD estimation in " + channel + " channel"
-    
+
     #Specify variable on which to fit
     if channel == "mu":
-        var = Variable("mt_mu", 0, 200, 20, "mtwMass", "m_{T }")    
+        var = Variable("mt_mu", 0, 200, 20, "mtwMass", "m_{T }")
     elif channel == "ele":
         var = Variable("met", 0, 200, 20, "MET", "MET")
     else:
-        print "Set either 'ele' or 'mu' as channel"
-        sys.exit(1)
-    
+        raise ValueError("channel must be 'mu' or 'ele': %s" % channel)
+    base_path = base_path + channel + "/"
+
     #Do you want to get the resulting yield after a cut on the fitted variable?
     #If yes, specify minumum value for the variable the cut. Obviously change to MET for electrons
     #Remember that the cut should be on the edge of 2 bins, otherwise the result will be inaccurate
@@ -80,13 +75,13 @@ def get_qcd_yield_with_selection(cuts, channel = "mu"):
     if channel == "mu":
         cutMT = True
         mtMinValue = 50.01 # M_T>50
-    if channel == "ele":
+    elif channel == "ele":
         cutMT = True
         mtMinValue = 45.01 # MET>45
-    
+
     #Use Default cuts for final selection. See FitConfig for details on how to change the cuts.
-    
-    
+
+
     if channel == "ele":
         cuts.setTrigger("1") #  || HLT_Ele27_WP80_v9==1 || HLT_Ele27_WP80_v8==1")
         cuts.setIsolationCut("el_mva > 0.9 & el_reliso < 0.1")
@@ -94,10 +89,10 @@ def get_qcd_yield_with_selection(cuts, channel = "mu"):
         cuts.setAntiIsolationCutUp("el_reliso > 0.11 & el_reliso < 0.55") # check +-10% variation
         cuts.setAntiIsolationCutDown("el_reliso > 0.09 & el_reliso < 0.45")
         lepton_weight = "*electron_triggerWeight*electron_IDWeight"
-    if channel == "mu":
+    elif channel == "mu":
         lepton_weight = "*muon_TriggerWeight*muon_IsoWeight*muon_IDWeight"
         #cuts.setTrigger("1")
-    
+
     cuts.setWeightMC("pu_weight*b_weight_nominal"+lepton_weight)
     #Recreate all necessary cuts after manual changes
     cuts.calcCuts()
@@ -112,9 +107,9 @@ def get_qcd_yield_with_selection(cuts, channel = "mu"):
     if channel == "ele":
         dataLumiIso = lumi_iso["ele"]
         dataLumiAntiIso = lumi_antiso["ele"]
-        
+
     lumis = DataLumiStorage(dataLumiIso, dataLumiAntiIso)
-    
+
     #Different groups are defined in init_data. Select one you need or define a new one.
     if channel == "mu":
         dataGroup = dgDataMuons
@@ -124,7 +119,7 @@ def get_qcd_yield_with_selection(cuts, channel = "mu"):
     #MC Default is a set muon specific groups with inclusive t-channel for now. MC Groups are without QCD
     #MCGroups = MC_groups_noQCD_InclusiveTCh
     MCGroups = MC_groups_noQCD_AllExclusive
-    
+
     #Do you want to get QCD template from MC?
     useMCforQCDTemplate = False
 
@@ -140,41 +135,54 @@ def get_qcd_yield_with_selection(cuts, channel = "mu"):
     #Generate path structure as base_path/iso/systematic, see util_scripts
     #If you have a different structure, change paths manually
 
-    if channel == "mu":
-        base_path = "/home/andres/single_top/stpol/out_step3_06_01"
-        #base_path = "~liis/SingleTopJoosep/stpol/out_step3_05_31_18_43/mu"        
-    if channel == "ele":
-        base_path = "~liis/SingleTopJoosep/stpol/out_step3_05_31_18_43/ele"        
+    #if channel == "mu":
+    #    base_path = "/home/andres/single_top/stpol/out_step3_06_01"
+    #    #base_path = "~liis/SingleTopJoosep/stpol/out_step3_05_31_18_43/mu"
+    #if channel == "ele":
+    #    base_path = "~liis/SingleTopJoosep/stpol/out_step3_05_31_18_43/ele"
 
     paths = generate_paths(systematics, base_path)
     #For example:
-    paths["iso"]["Nominal"] = base_path+"/iso/Nominal/"
-    paths["antiiso"]["Nominal"] = base_path+"/antiiso/Nominal/"
-    #Then open files    
+    paths["iso"]["Nominal"] = base_path+"/iso/nominal/"
+    paths["antiiso"]["Nominal"] = base_path+"/antiiso/nominal/"
+    #Then open files
     print "opening data and MC files"
     openedFiles = open_all_data_files(dataGroup, MCGroups, QCDGroup, paths)
-    
+
     #Before Running make sure you have 'templates' and 'fits' subdirectories where you're running
     #Root files with templates and fit results will be saved there.
-    #Name from FitConfig will be used in file names 
-           
+    #Name from FitConfig will be used in file names
+
     return get_qcd_yield_with_fit(var, cuts, cutMT, mtMinValue, dataGroup, lumis, MCGroups, systematics, openedFiles, useMCforQCDTemplate, QCDGroup)
 
 #Run as /scratch/mario/theta/utils2/theta-auto.py get_qcd_yield.py - only works in phys!
 if __name__=="__main__":
-    lepton = "mu"
-    #lepton = "ele"
-    
-    cuts_final = FitConfig( "final_selection" )
-    cuts_2j0t = FitConfig( "2j0t_selection" )
+    try:
+        sys.path.append(os.environ["STPOL_DIR"] )
+    except KeyError as e:
+        print "Could not find the STPOL_DIR environment variable, did you run `source setenv.sh` in the code base directory?"
+        raise e
+
+    cuts_final = FitConfig( "final_selection", trigger="1.0")
+    cuts_2j0t = FitConfig( "2j0t_selection", trigger="1.0")
     cuts_2j0t.setBaseCuts("n_jets == 2 && n_tags == 0")
-    cuts_final_without_eta = FitConfig( "final_selection_without_eta_cut" )
+    cuts_final_without_eta = FitConfig( "final_selection_without_eta_cut", trigger="1.0")
     cuts_final_without_eta.setFinalCuts("top_mass < 220 && top_mass > 130")
-        
-    #cuts = cuts_final
-    cuts = cuts_final_without_eta
-    #cuts = cuts_2j0t
-    ((y, error), fit) = get_qcd_yield_with_selection(cuts, lepton)
+
+    cuts = {}
+    cuts["final"] = cuts_final
+    cuts["2j0t"] = cuts_2j0t
+    cuts["final_without_eta"] = cuts_final_without_eta
+
+    import argparse
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--lepton', dest='lepton', choices=["mu", "ele"], required=True, help="The lepton channel used for the fit")
+    parser.add_argument('--cut', dest='cut', choices=cuts.keys(), required=True, help="The cut region to use in the fit")
+    parser.add_argument('--doSystematics', dest='doSystematics', action="store_true", default=False)
+    args = parser.parse_args()
+
+    cut = cuts[args.cut]
+    ((y, error), fit) = get_qcd_yield_with_selection(cut, args.lepton, do_systematics=args.doSystematics)
     #print cuts
     print "Selection: %s" % cuts.name
     print "QCD yield in selected region: %.2f +- %.2f, ratio to template from ONLY data %.3f" % (y, error, y/fit.orig["qcd_no_mc_sub"])
@@ -184,5 +192,4 @@ if __name__=="__main__":
 
     print "Fit info:"
     print fit
-    #make plot
     plot_fit(fit.var, cuts, fit.dataHisto, fit)
