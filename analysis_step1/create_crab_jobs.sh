1e218f57b601e9dcb5e6d04c8efec381bb7e23e6
jpata <joosep.pata@gmail.com>
2013-07-04 17:20:11 +0300

    data is local

--- analysis_step1/create_crab_jobs.sh
+++ analysis_step1/create_crab_jobs.sh
@@ -4,6 +4,6 @@ timestamp=`eval date +%m_%d`_$TAG
 OFDIR=$STPOL_DIR/crabs/step1_$timestamp
 python $STPOL_DIR/util/datasets.py -t stpol_step1_$timestamp -T $STPOL_DIR/crabs/crab_MC_step1.cfg -d S1_MC -o $OFDIR/mc/nominal
 python $STPOL_DIR/util/datasets.py -t stpol_step1_$timestamp -T $STPOL_DIR/crabs/crab_MC_step1_glideInRemote.cfg -d S1_MC_syst -o $OFDIR/mc/systematic
-python $STPOL_DIR/util/datasets.py -t stpol_step1_$timestamp -T $STPOL_DIR/crabs/crab_Data_step1_glideInRemote.cfg -d S1_D -o $OFDIR/data
+python $STPOL_DIR/util/datasets.py -t stpol_step1_$timestamp -T $STPOL_DIR/crabs/crab_Data_step1.cfg -d S1_D -o $OFDIR/data
 python $STPOL_DIR/util/datasets.py -t stpol_step1_$timestamp -T $STPOL_DIR/crabs/crab_MC_step1_glideInRemote.cfg -d S1_FSIM_WJ -o $OFDIR/mc/fastsim
 
