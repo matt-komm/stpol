c9ff6cb9b7f4deb30d8ffa1b858aa830162836f6
Joosep Pata <joosep.pata@gmail.com>
2013-07-04 10:46:53 +0300

    comments

--- control_plots/muon_channel.py
+++ control_plots/muon_channel.py
@@ -26,19 +26,29 @@ if __name__=="__main__":
     #tdrstyle.tdrstyle()
 
     datadirs = dict()
+    
+    #This symlink is present in the repo for *.hep.kbfi.ee, if running on other machines, you must make it yourself by doing
+    #ln -s /path/to/step3/out $STPOL_DIR/step3_latest
+    #isolated files are by default in $STPOL_DIR/step3_latest/mu/iso/nominal/*.root
     datadirs["iso"] = "/".join((os.environ["STPOL_DIR"], "step3_latest", "mu" ,"iso", "nominal"))
+    #Use the anti-isolated data for QCD $STPOL_DIR/step3_latest/mu/antiiso/nominal/SingleMu.root
     datadirs["antiiso"] = "/".join((os.environ["STPOL_DIR"], "step3_latest", "mu" ,"antiiso", "nominal"))
 
+    #Load all the samples in the isolated directory
     samples = Sample.fromDirectory(datadirs["iso"], out_type="dict")
     samples["SingleMu_aiso"] = Sample.fromFile(datadirs["antiiso"] + "/SingleMu.root")
     
     hists_mc = dict()
     hist_data = None
+    
+    #Define the variable, cut, weight and lumi
     var = "cos_theta"
     cut_name = "2j1t"
     cut_str = str(Cuts.n_jets(2)*Cuts.n_tags(1)*Cuts.lepton_veto*Cuts.one_muon*Cuts.mt_mu*Cuts.top_mass_sig*Cuts.eta_lj)
     weight_str = "1.0"
-    lumi = 20000
+    lumi = 20000 #FIXME: take from the step2 output luminosity.txt
+    
+    #nbins, min, max
     plot_range= [20, -1, 1]
     
     for name, sample in samples.items():
@@ -56,7 +66,10 @@ if __name__=="__main__":
             #hist_qcd.
             pass
 
+    #Combine the subsamples to physical processes
     merged_hists = merge_hists(hists_mc, merge_cmds).values()
+    
+    #Some printout
     for h in merged_hists + [hist_data]:
         print h.GetName(), h.GetTitle(), h.Integral()
 
@@ -68,6 +81,7 @@ if __name__=="__main__":
     stacks = plot_hists_stacked(canv, stacks_d)
     canv.Draw()
 
+    #Create the dir if it doesn't exits
     try:
         os.mkdir("muon_out")
     except OSError:
