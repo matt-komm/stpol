b8a430ea46a7182a53920348e1314bcddcbe4553
jpata <joosep.pata@gmail.com>
2013-07-05 12:14:33 +0300

    milestone

--- common/global_tags.py
+++ common/global_tags.py
@@ -5,6 +5,16 @@ global_tags = dict()
 global_tags["data"] = dict()
 global_tags["mc"] = dict()
 
-global_tags["mc"]["nominal_Summer12_DR53X"] = "START53_V20::All"
-global_tags["data"]["22Jan_ReReco_Runs2012ABCD"] = "FT_53_V21_AN4::All"
+gt_mc = "START53_V20::All"
+gt_data = "FT_53_V21_AN4::All"
+
+#step1
+global_tags["mc"]["nominal_Summer12_DR53X"] = gt_mc
+global_tags["mc"]["systematic_Summer12_DR53X"] = gt_mc
+global_tags["data"]["22Jan_ReReco_Runs2012ABCD"] = gt_data
+
+#step2
+global_tags["mc"]["Apr19"] = gt_mc
+global_tags["mc"]["Apr19_qcd"] = gt_mc
+global_tags["data"]["May20"] = gt_data
 
