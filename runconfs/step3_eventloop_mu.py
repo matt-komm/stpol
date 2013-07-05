b79e4f235a8ceb2696ba12c1c8e2b0ff04f9f545
jpata <joosep.pata@gmail.com>
2013-07-05 16:04:16 +0300

    runconfs_rewrite

--- runconfs/step3_eventloop_mu.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from runconfs.step3_eventLoop_cfg import *
-process.muonCuts.requireOneMuon = cms.bool(True)
-process.muonCuts.requireOneElectron = cms.bool(False)
-process.HLTmu.cutOnHLT = cms.bool(True)
