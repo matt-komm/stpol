905bd70fee6f54f4f69030534b650e7b002fb015
jpata <joosep.pata@gmail.com>
2013-07-09 14:31:19 +0300

    printout

--- util/test_step3_signal.sh
+++ util/test_step3_signal.sh
@@ -8,6 +8,6 @@ if [ -d "$OFDIR" ]; then
     rm -Rf "$OFDIR"
 fi
 mkdir -p "$OFDIR"
-echo "Calling ""$CMSSW_BASE"/bin/"$SCRAM_ARCH"/Step3_EventLoop
+echo "Calling ""$CMSSW_BASE"/bin/"$SCRAM_ARCH"/Step3_EventLoop with output to $OFDIR
 head -n5 filelists/step2/latest/iso/nominal/mc/T_t_ToLeptons.txt |  "$CMSSW_BASE"/bin/"$SCRAM_ARCH"/Step3_EventLoop "$CMSSW_BASE""/src/SingleTopPolarization/Analysis/python/runconfs/step3/test.py" --doControlVars --isMC --outputFile="$OFDIR"/out.root --cutString="mu_pt>50 && abs(eta_lj)>2.5" &> "$OFDIR"/log_step3.txt
 tail -n10 "$OFDIR"/log_step3.txt
