93ef949184b1486b85c186e7ed245257b855e968
jpata <joosep.pata@gmail.com>
2013-07-09 13:44:25 +0300

    added small script to test step3

---
+++ util/test_step3_signal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Running step3 test"
+cd "$STPOL_DIR"
+source setenv.sh
+OFDIR="$STPOL_DIR"/testing/step3/signal
+if [ -d "$OFDIR" ]; then
+    echo "removing '"$OFDIR"'"
+    rm -Rf "$OFDIR"
+fi
+mkdir -p "$OFDIR"
+echo "Calling ""$CMSSW_BASE"/bin/"$SCRAM_ARCH"/Step3_EventLoop
+head -n5 filelists/step2/latest/iso/nominal/mc/T_t_ToLeptons.txt |  "$CMSSW_BASE"/bin/"$SCRAM_ARCH"/Step3_EventLoop "$CMSSW_BASE""/src/SingleTopPolariation/Analysis/python/runconfs/step3/test.py" --doControlVars --isMC --outputFile="$OFDIR"/out.root --cutString="mu_pt>50 && abs(eta_lj)>2.5" &> "$OFDIR"/log_step3.txt
+tail -n10 "$OFDIR"/log_step3.txt
