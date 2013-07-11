2b62af3cb584b9e75642a9a18ec40fd9d1d3ecea
Joosep Pata <joosep.pata@gmail.com>
2013-07-11 16:40:05 +0200

    copy all

--- sync/copy_step3.sh
+++ sync/copy_step3.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-rscp cms.hep.kbfi.ee:~/singletop/stpol2/sync/exclusive/step3.root exclusive/
-rscp cms.hep.kbfi.ee:~/singletop/stpol2/sync/inclusive/step3.root inclusive/
-rscp cms.hep.kbfi.ee:~/singletop/stpol2/sync/exclusive/log*.txt exclusive/
-rscp cms.hep.kbfi.ee:~/singletop/stpol2/sync/inclusive/log*.txt inclusive/
+rscp -r cms.hep.kbfi.ee:~/singletop/stpol/sync/exclusive exclusive/
+rscp -r cms.hep.kbfi.ee:~/singletop/stpol/sync/inclusive inclusive/
