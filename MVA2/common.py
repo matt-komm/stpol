5dd6d2d485b8c2fdd0404bc9a5e5d8d61844d345
Ants Remm <ants.remm@cern.ch>
2013-07-12 14:10:38 +0200

    add first step (prepare root files) to common.py

--- MVA2/common.py
+++ MVA2/common.py
@@ -2,7 +2,8 @@ import pickle
 import ROOT
 from plots.common import cross_sections
 from plots.common import cuts
-
+import array
+import random
 
 rootfilepath = "step3_latest/"
 
@@ -133,3 +134,93 @@ class MVA_trainer:
 			pickle.dump(meta, pklfile)
 			pklfile.close()
 		self.tempfile.Close()
+
+def prepare_files(signals, backgrounds, ofname = "prepared.root", cutstring = str(cuts.Cuts.rms_lj*cuts.Cuts.mt_mu*cuts.Cuts.n_jets(2)*cuts.Cuts.n_tags(1)), default_ratio = 0.5, lept = "mu"):
+	if not isinstance(signals, dict):
+		if isinstance(signals, list):
+			d = {}
+			for ch in signals:
+				d[ch] = default_ratio
+			signals = d
+		else:
+			print "prepare_files: argument signals of invalid type", type(signals)
+			return
+	else:
+		for ch in signals:
+			if signals[ch] == None:
+				signals[ch] = default_ratio
+	
+	
+	if not isinstance(backgrounds, dict):
+		if isinstance(backgrounds, list):
+			d = {}
+			for ch in backgrounds:
+				d[ch] = default_ratio
+			backgrounds = d
+		else:
+			print "prepare_files: argument backgrounds of invalid type", type(backgrounds)
+			return
+	else:
+		for ch in backgrounds:
+			if backgrounds[ch] == None:
+				backgrounds[ch] = default_ratio
+	
+		
+	ofile = ROOT.TFile(ofname, "RECREATE")
+	ofile.mkdir("train/signal")
+	ofile.mkdir("train/background")
+	ofile.mkdir("test/signal")
+	ofile.mkdir("test/background")
+	
+	meta = {"lept": lept, "initial_events": {}, "cutstring": cutstring}
+	
+	
+	for ch in signals.keys() + backgrounds.keys():
+		ifname = rootfilepath + lept + "/iso/nominal/" + ch + ".root"
+		ifile = ROOT.TFile(ifname)
+		meta["initial_events"][ch] = ifile.Get("trees/count_hist").GetBinContent(1)
+		tree = ifile.Get("trees/Events").CopyTree(cutstring)
+		
+		tr = array.array('i', [0])
+		
+		newbranch = tree.Branch("training", tr, "training/I")
+		nentries = tree.GetEntries()
+		
+		
+		ratio = signals[ch] if ch in signals else backgrounds[ch]
+		
+		for n in range(nentries):
+			tree.GetEntry(n)
+			tr[0] = 1 if random.random() >= ratio else 0
+			newbranch.Fill()
+		
+		ofile.cd("train/signal" if ch in signals else "train/background")
+		tree_train = tree.CopyTree("(training == 1)")
+		tree_train.Write(ch)
+		
+		ofile.cd("test/signal" if ch in signals else "test/background")
+		tree_test = tree.CopyTree("(training == 0)")
+		tree_test.Write(ch)
+	
+	ofile.cd("")
+	ofile.WriteTObject(ROOT.TObjString(pickle.dumps(meta)), 'meta')
+	
+	ofile.Close()
+	
+	
+if __name__ == "__main__":
+	prepare_files(["T_t_ToLeptons", "Tbar_t_ToLeptons"], ["WJets_inclusive"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
