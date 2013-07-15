50df61e82e866b9d085618abdc36ea8713e6aa87
Mario Kadastik <mario@cern.ch>
2013-07-15 21:20:53 +0300

    Added some further cuts

--- MVA/skimStep3ForMVA.py
+++ MVA/skimStep3ForMVA.py
@@ -16,9 +16,9 @@ else:
 
 dir=sys.argv[1]
 print "Using:",dir
-basecut = 'deltaR_bj > 0.3 & deltaR_lj > 0.3 & n_jets == 2 & n_tags == 1 & n_veto_ele == 0 & n_veto_mu == 0'
-mucut = basecut+' & n_muons == 1 & mu_pt > 26'
-elecut = basecut+' & n_eles == 1 & el_mva > 0.9 & el_pt > 30'
+basecut = 'deltaR_bj > 0.3 & deltaR_lj > 0.3 & n_jets == 2 & n_tags == 1 & n_veto_ele == 0 & n_veto_mu == 0 & & rms_lj < 0.025 & pt_lj > 40 & pt_bj > 40'
+mucut = basecut+' & n_muons == 1 & mu_pt > 26 '
+elecut = basecut+' & n_eles == 1 & el_pt > 30 & ( (HLT_Ele27_WP80_v8 == 1 ) || (HLT_Ele27_WP80_v9 == 1) || (HLT_Ele27_WP80_v10==1) || (HLT_Ele27_WP80_v11==1) )'
 cut = elecut
 if '/mu/' in dir:
     cut = mucut
