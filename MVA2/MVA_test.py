841b725f77b0569dd2b2afe2b61a210100684ac5
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-07-16 23:03:13 +0200

    Make the test MVA use CLI arguments for the file list.

--- MVA2/MVA_test.py
+++ MVA2/MVA_test.py
@@ -3,19 +3,26 @@ from plots.common import cross_sections
 from plots.common import cuts
 import common
 
+def applyMVA(fName='prepared.root', jobname='quicktest'):
+	train = common.MVA_trainer(fName, jobname=jobname)
+	train.add_variable("eta_lj")
+	train.add_variable("top_mass")
+	#train.prepare()
 
-train = common.MVA_trainer('prepared.root', jobname='quicktest')
-train.add_variable("eta_lj")
-train.add_variable("top_mass")
-#train.prepare()
+	#train.get_factory().PrepareTrainingAndTestTree(ROOT.TCut(), '')
 
-#train.get_factory().PrepareTrainingAndTestTree(ROOT.TCut(), '')
+	train.book_method(ROOT.TMVA.Types.kMLP, "MLP", "!H:!V:VarTransform=N:HiddenLayers=2:TrainingMethod=BFGS:NCycles=2")
+	train.book_method(ROOT.TMVA.Types.kBDT, "BDT", "!H:!V:NTrees=20:nEventsMin=20:MaxDepth=2:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=10:PruneMethod=NoPruning")
 
-train.book_method(ROOT.TMVA.Types.kMLP, "MLP", "!H:!V:VarTransform=N:HiddenLayers=2:TrainingMethod=BFGS:NCycles=2")
-train.book_method(ROOT.TMVA.Types.kBDT, "BDT", "!H:!V:NTrees=20:nEventsMin=20:MaxDepth=2:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=10:PruneMethod=NoPruning")
+	train.get_factory().TrainAllMethods()
 
-train.get_factory().TrainAllMethods()
+	train.evaluate()
+	train.pack()
+	train.finish()
 
-train.evaluate()
-#train.pack()
-#train.finish()
+import sys
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		applyMVA(sys.argv[1])
+	else:
+		print 'File not given (or too many given).'
