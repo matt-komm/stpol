bfb178398ece2462ba45bb82a4a10bd1ff0e2afa
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-07-17 22:55:10 +0200

    Update the preliminary stacked plot.

--- MVA2/plotstack.py
+++ MVA2/plotstack.py
@@ -1,26 +1,37 @@
-import argparse, glob, os, os.path, re
+import argparse, glob, sys, os, os.path, re
 import ROOT
 import common
-#import plots.common.cross_sections
-import plots.common.colors
-import plots.common.cuts
+import plots.common.colors, plots.common.cuts
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', default='step3_latest/mu/iso/nominal', help = 'input dir')
+parser.add_argument('-v', '--var', default='costheta', help = 'plot variable (costheta, bdt)')
 args = parser.parse_args()
 
-cut = '((((rms_lj < 0.025) && (mt_mu > 50)) && (n_jets == 2)) && (n_tags == 1))'
-#cut = str(plots.common.cuts.Cuts.final(2, 1)); print 'Cut:', cut
-var = 'mva_BDT'
-histparam = (10, 0.4, 0.9)
+# Default plot = costheta
+if args.var=='costheta':
+	var = 'cos_theta'
+	cut = str(plots.common.cuts.Cuts.final(2, 1))
+	histparam = (20, 0, 1)
+elif args.var=='bdt':
+	cut = '((((rms_lj < 0.025) && (mt_mu > 50)) && (n_jets == 2)) && (n_tags == 1))'
+	var = 'mva_BDT'
+	histparam = (10, 0.4, 0.9)
+else:
+	print 'Bad --var: (%s)'%args.var
+	exit(1)
+
+print 'Var:', var
+print 'Cut:', cut
+print 'Hpa:', histparam
 
 infiles = glob.glob(os.path.join(args.dir, '*'))
 
 def parseTree(chName, data=False):
+	print 'Sample:', chName
+	
 	ret = {}
-	#ret['name']  = re.search('([A-Za-z0-9_]*)\.root$', fName).group(1)
 	ret['name'] = chName
-	#print 'Sample:', ret['name']
 	ret['fName'] = os.path.join(args.dir, chName+'.root')
 	ret['tfile'] = ROOT.TFile(ret['fName'])
 	ret['tree'] = ret['tfile'].Get('trees/Events')
@@ -39,11 +50,13 @@ print 'Backgrounds:', bgnames
 bgs = map(parseTree, bgnames)
 dt = parseTree('SingleMu', data=True)
 
-#stack = ROOT.THStack('stack', var)
 stack = common.HStack('stack', var)
 for bg in bgs:
 	bg['h'].SetFillColor(plots.common.colors.sample_colors_same[bg['name']])
 	stack.Add(bg['h'])
+
+maxval = max(dt['h'].GetMaximum(), stack.GetMaximum())
+stack.SetMaximum(maxval)
 
 cvs = ROOT.TCanvas()
 stack.Draw()
