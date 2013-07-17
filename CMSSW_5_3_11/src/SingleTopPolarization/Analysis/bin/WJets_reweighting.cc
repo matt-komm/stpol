7c87f4d1c9a77a517fbf19b599384cd1eb1c9899
Joosep Pata <joosep.pata@gmail.com>
2013-07-18 00:45:56 +0200

    changed classifications

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/WJets_reweighting.cc
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/WJets_reweighting.cc
@@ -10,14 +10,17 @@
 
 //TFile global("global.root", "RECREATE");
 
-enum WJetsClassification {
-    //W+heavy
+enum WJetsClassification0 {
+    //W_QQ
     Wbb,
     Wcc,
+    Wbc,
+
+    //W_Qq
     WbX,
     WcX,
 
-    //W+light
+    //W_qq
     WgX,
     Wgg,
     WXX,
@@ -28,12 +31,29 @@ enum WJetsClassification1 {
     WJETS1_W_light,
 };
 
+enum WJetsClassification2 {
+    WJETS2_W_QQ,
+    WJETS2_W_Qq,
+    WJETS2_W_qq,
+};
+
 int classify_1(int cls) {
-    if (cls == Wbb || cls==Wcc || cls==WbX || cls==WcX) {
+    if (cls == Wbb || cls==Wcc || cls==WbX || cls==WcX || cls==Wbc) {
         return WJETS1_W_heavy;
     }
     else {
         return WJETS1_W_light;
+    }
+}
+
+int classify_2(int cls) {
+    if (cls == Wbb || cls==Wcc || cls==Wbc) {
+        return WJETS2_W_QQ;
+    }
+    else if(cls == WcX || cls==WbX) {
+        return WJETS2_W_Qq;
+    } else {
+        return WJETS2_W_qq;
     }
 }
 
@@ -46,41 +66,47 @@ std::string classify_1_str(int cls) {
         throw 1;
 }
 
-static const float Wlight_sf = 1.07267761;
-void weight_1(int cls, float cos_theta, std::map<int, TH1F*>& hists, float& w, float& w_up, float& w_down) {
-    w = 1.0;
-    w_up = 1.0;
-    w_down = 1.0;
+static const float Wlight_sf = 1.0991871445834183;
+void weight(int cls, float cos_theta, TH1F* hist,
+    float& w_flat, float& w_flat_up, float& w_flat_down,
+    float& w_shape, float& w_shape_up, float& w_shape_down) {
+    w_flat = 1.0;
+    w_flat_up = 1.0;
+    w_flat_down = 1.0;
+
+    w_shape = 1.0;
+    w_shape_up = 1.0;
+    w_shape_down = 1.0;
+
     int cls_simple = classify_1(cls);
 
-    TH1F* h = hists[cls_simple];
-    int bin = h->FindBin(cos_theta);
+    int bin = hist->FindBin(cos_theta);
     
     //std::cout << "cos_theta=" << cos_theta << " Bin=" << bin << " cls=" << cls << " cls_1=" << cls_simple << " w=" << h->GetBinContent(bin) << std::endl;
     if (cls_simple==WJETS1_W_heavy) { //W+heavy
-        w *= 1.0;
-        w_up *= 2.0;
-        w_down *= 0.5;
+        w_flat *= 1.0;
+        w_flat_up *= 2.0;
+        w_flat_down *= 0.5;
     }
     else if (cls_simple == WJETS1_W_light) { //W+light
 
         //madgraph W+light, measured in 2J data/madgraph
-        w *= Wlight_sf;
-        w_up *= Wlight_sf+(Wlight_sf-1.0);
-        w_down *= Wlight_sf-(Wlight_sf-1.0);
+        w_flat *= Wlight_sf;
+        w_flat_up *= Wlight_sf+(Wlight_sf-1.0);
+        w_flat_down *= Wlight_sf-(Wlight_sf-1.0);
     }
     else 
         throw 1;
 
     //Measured shape differences sherpa vs madgraph
-    w *= h->GetBinContent(bin);
-    w_up *= (h->GetBinContent(bin) + h->GetBinError(bin));
-    w_down *= (h->GetBinContent(bin) - h->GetBinError(bin));
+    w_shape *= hist->GetBinContent(bin);
+    w_shape_up *= (hist->GetBinContent(bin) + hist->GetBinError(bin));
+    w_shape_down *= (hist->GetBinContent(bin) - hist->GetBinError(bin));
     //std::cout << "w=" << w << std::endl;
 
 }
 
-WJetsClassification classify(int flavour_a, int flavour_b) {
+WJetsClassification0 classify(int flavour_a, int flavour_b) {
     int a = abs(flavour_a);
     int b = abs(flavour_b);
     if (a==5 && b==5) {
@@ -92,38 +118,21 @@ WJetsClassification classify(int flavour_a, int flavour_b) {
     else if (a==4 && b==4) {
         return Wcc;
     }
+    else if ((a==5 && b==4) || (a==4 && b==5)) {
+        return Wbc;
+    }
     else if (a==5 || b==5) {
         return WbX;
-    }
-    else if (a==21 || b==21) {
-        return WgX;
     }
     else if (a==4 || b==4) {
         return WcX;
     }
+    else if (a==21 || b==21) {
+        return WgX;
+    }
     else {
         return WXX;
     }
-}
-
-TH1F* get_ratio_hist_1(TFile* fi, int cls) {
-
-
-    const std::string mg("WJets/madgraph/");
-    const std::string sh("WJets/sherpa/");
-    const std::string flavour(classify_1_str(cls));
-
-    TH1F* h_mg = (TH1F*)fi->Get((mg+flavour).c_str());
-    TH1F* h_sh = (TH1F*)fi->Get((sh+flavour).c_str());
-    //global.cd();
-    std::string ratio_name(std::string("ratio__") + flavour);
-    fi->Delete((ratio_name + std::string(";*")).c_str());
-    TH1F* ratio = (TH1F*)h_sh->Clone(ratio_name.c_str());
-    ratio->Divide(h_mg);
-    ratio->Write();
-    //fi->Close();
-    return ratio;
-
 }
 
 int main(int argc, char* argv[]) {
@@ -154,15 +163,15 @@ int main(int argc, char* argv[]) {
     TTree* weight_tree = new TTree("WJets_weights", "WJets_weights");
 
     TFile* hists_fi = new TFile("$STPOL_DIR/CMSSW_5_3_11/src/data/WJets_reweighting/hists__costheta_flavours_merged.root", "UPDATE");
+    
     std::map<int, TH1F*> ratio_hists;
-    ratio_hists[WJETS1_W_heavy] = get_ratio_hist_1(hists_fi, WJETS1_W_heavy);
-    ratio_hists[WJETS1_W_light] = get_ratio_hist_1(hists_fi, WJETS1_W_light);
-
-    ratio_hists[WJETS1_W_heavy]->Print("ALL");
-    ratio_hists[WJETS1_W_light]->Print("ALL");
+    ratio_hists[WJETS2_W_QQ] = (TH1F*)hists_fi->Get("ratio__W_QQ");
+    ratio_hists[WJETS2_W_Qq] = (TH1F*)hists_fi->Get("ratio__W_Qq");
+    ratio_hists[WJETS2_W_qq] = (TH1F*)hists_fi->Get("ratio__W_qq");
 
     int gen_flavour_bj = -1;
     int gen_flavour_lj = -1;
+    int n_tags = -1;
     float cos_theta = 0.0;
     
     events->SetBranchStatus("*", 0);
@@ -172,32 +181,47 @@ int main(int argc, char* argv[]) {
     events->SetBranchAddress("gen_flavour_bj", &gen_flavour_bj);
     events->SetBranchAddress("gen_flavour_lj", &gen_flavour_lj);
     events->SetBranchAddress("cos_theta", &cos_theta);
+    events->SetBranchAddress("n_tags", &n_tags);
     
     //float wjets_flavour_weight = 1.0;
-    int cls = -1;
+    int cls0 = -1, cls1=-1, cls2=-1;
 
-    float weight=1.0, weight_up=1.0, weight_down = 1.0;
-    TBranch* weight_branch = weight_tree->Branch("wjets_mg_flavour_weight", &weight, "wjets_mg_flavour_weight/F"); 
-    //events->SetBranchStatus("wjets_mg_flavour_weight", 1);
-    TBranch* weight_branch_up = weight_tree->Branch("wjets_mg_flavour_weight_up", &weight_up, "wjets_mg_flavour_weight_up/F"); 
-    //events->SetBranchStatus("wjets_mg_flavour_weight_up", 1);
-    TBranch* weight_branch_down = weight_tree->Branch("wjets_mg_flavour_weight_down", &weight_down, "wjets_mg_flavour_weight_down/F"); 
-    //events->SetBranchStatus("wjets_mg_flavour_weight_down", 1);
+    std::map<const std::string, float> weights_flat;
+    std::map<const std::string, float> weights_shape;
     
-    TBranch* cls_branch = weight_tree->Branch("wjets_flavour_classification", &cls, "wjets_flavour_classification/I"); 
-    //events->SetBranchStatus("wjets_flavour_classification", 1);
+    weights_flat["nominal"] = 1.0;
+    weights_flat["up"] = 1.0;
+    weights_flat["down"] = 1.0;
+
+    weights_shape["nominal"] = 1.0;
+    weights_shape["up"] = 1.0;
+    weights_shape["down"] = 1.0;
+    
+    weight_tree->Branch("wjets_mg_flavour_flat_weight", & weights_flat["nominal"], "wjets_mg_flavour_flat_weight/F"); 
+    weight_tree->Branch("wjets_mg_flavour_flat_weight_up", &weights_flat["up"], "wjets_mg_flavour_flat_weight_up/F"); 
+    weight_tree->Branch("wjets_mg_flavour_flat_weight_down", &weights_flat["down"], "wjets_mg_flavour_flat_weight_down/F"); 
+    
+    weight_tree->Branch("wjets_mg_flavour_shape_weight", & weights_shape["nominal"], "wjets_mg_flavour_shape_weight/F"); 
+    weight_tree->Branch("wjets_mg_flavour_shape_weight_up", &weights_shape["up"], "wjets_mg_flavour_shape_weight_up/F"); 
+    weight_tree->Branch("wjets_mg_flavour_shape_weight_down", &weights_shape["down"], "wjets_mg_flavour_shape_weight_down/F"); 
+
+    TBranch* cls0_branch = weight_tree->Branch("wjets_flavour_classification0", &cls0, "wjets_flavour_classification0/I"); 
+    TBranch* cls1_branch = weight_tree->Branch("wjets_flavour_classification1", &cls1, "wjets_flavour_classification1/I"); 
+    TBranch* cls2_branch = weight_tree->Branch("wjets_flavour_classification2", &cls2, "wjets_flavour_classification2/I"); 
     
     int Nbytes = 0;
 
     std::cout << "Beginning event loop over " << events->GetEntries() << " events." << std::endl;
      for (int n=0; n<events->GetEntries(); n++) {
         events->GetEntry(n);
-        weight=1.0;
-        weight_up=1.0;
-        weight_down = 1.0;
-        cls = classify(gen_flavour_bj, gen_flavour_lj);
-        //wjets_flavour_weight = weights[cls];
-        weight_1(cls, cos_theta, ratio_hists, weight, weight_up, weight_down);
+        cls0 = classify(gen_flavour_bj, gen_flavour_lj);
+        cls1 = classify_1(cls0);
+        cls2 = classify_2(cls0);
+
+        weight(cls0, cos_theta, ratio_hists[cls2],
+            weights_flat["nominal"], weights_flat["up"], weights_flat["down"],
+            weights_shape["nominal"], weights_shape["up"], weights_shape["down"]);
+
         int nbytes = weight_tree->Fill();
         if (nbytes<0) {
             std::cerr << "Write error!" << std::endl;
