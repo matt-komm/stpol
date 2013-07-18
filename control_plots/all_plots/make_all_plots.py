591ee04453040b3c85e7a17adc0e30efd678fd50
Mario Kadastik <mario@cern.ch>
2013-07-18 12:48:29 +0300

    Added QCD scale factors, refactored cuts

--- control_plots/all_plots/make_all_plots.py
+++ control_plots/all_plots/make_all_plots.py
@@ -35,8 +35,9 @@ data_fn = 'Single'+proc.title()
 del merge_cmds['data']
 flist=sum(merge_cmds.values(),[])
 tree='Events'
-from plot_defs import plot_defs
+from plot_defs import *
 
+mc_sf=1.15
 lumiele=6144
 lumimu=6398
 lumi = lumiele
@@ -68,11 +69,11 @@ for pd in keylist:
     if not plot_defs[pd]['enabled']:
         continue
     var = plot_defs[pd]['var']
-    cut = plot_defs[pd]['commonCut']
+    cut = None
     if proc == 'ele':
-        cut *= plot_defs[pd]['elecut']
+        cut = plot_defs[pd]['elecut']
     if proc == 'mu':
-        cut *= plot_defs[pd]['mucut']
+        cut = plot_defs[pd]['mucut']
 
     cut_str = str(cut)
     weight_str = "SF_total"
@@ -83,7 +84,7 @@ for pd in keylist:
         print "Starting:",name
         if sample.isMC:
             hist = sample.drawHistogram(var, cut_str, weight=weight_str, plot_range=plot_range)
-            hist.hist.Scale(sample.lumiScaleFactor(lumi))
+            hist.hist.Scale(sample.lumiScaleFactor(lumi)*mc_sf)
             hists_mc[sample.name] = hist.hist
             Styling.mc_style(hists_mc[sample.name], sample.name)
         elif name[0:6] == "Single":
@@ -97,9 +98,9 @@ for pd in keylist:
             if proc == 'ele':
                 cv='el_reliso'
                 lb=0.1
-            qcd_cut = cut*Cut(cv+'>'+str(lb)+' & '+cv+'<0.5')
+            qcd_cut = cut*Cut('deltaR_lj>0.3 && deltaR_bj>0.3 && '+cv+'>'+str(lb)+' & '+cv+'<0.5')
             hist_qcd = sample.drawHistogram(var, str(qcd_cut), weight="1.0", plot_range=plot_range).hist
-            #hist_qcd.Scale(0.4)
+            hist_qcd.Scale(qcdScale[proc][plot_defs[pd]['estQcd']])
             hists_mc['QCD'] = hist_qcd
             hists_mc['QCD'].SetTitle('QCD')
             Styling.mc_style(hists_mc['QCD'], 'QCD')
@@ -127,7 +128,7 @@ for pd in keylist:
     boxloc = 'top-right'
     if plot_defs[pd]['labloc'] == 'top-right':
         boxloc = 'top-left'
-    lbox = lumi_textbox(lumi,"top-left")
+    lbox = lumi_textbox(lumi,boxloc)
     lbox.Draw()
     leg.Draw()
     canv.Draw()
