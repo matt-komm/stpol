1049d7babb0a6cfb5150be29c6e1e69056a7b907
Joosep Pata <joosep.pata@gmail.com>
2013-07-22 16:57:38 +0200

    removed ttree selection

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/runconfs/step3/base_nocuts.py
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/runconfs/step3/base_nocuts.py
@@ -79,11 +79,11 @@ parser.add_option("--skipTree", dest="skipTree", action="store_true", default=Fa
 parser.add_option("--outputFile", dest="outputFile", type="string", default="step3.root",
     description="Filename of the flat ROOT output file."
 )
-parser.add_option("--cutString", dest="cutString", type="string", default="1.0",
-    description="An additional TTree called Events_selected will be created by applying this cutstring.\
-    Note that applying this cut string will NOT decrease processing time by skipping some events,\
-    which is conceptually different from the above cuts."
-)
+# parser.add_option("--cutString", dest="cutString", type="string", default="1.0",
+#     description="An additional TTree called Events_selected will be created by applying this cutstring.\
+#     Note that applying this cut string will NOT decrease processing time by skipping some events,\
+#     which is conceptually different from the above cuts."
+# )
 
 options, args = parser.parse_args()
 
@@ -122,7 +122,7 @@ print "Output file: %s" % options.outputFile
 
 process.fwliteOutput = cms.PSet(
     fileName = cms.string(options.outputFile),
-    cutString = cms.string(options.cutString)
+    #cutString = cms.string(options.cutString)
 )
 
 process.muonCuts = cms.PSet(
@@ -283,7 +283,7 @@ process.metCuts = cms.PSet(
     )
 
 process.evtShapeVars = cms.PSet(
-    doEvtShapeVars = cms.bool(options.doEventShape),
+    doEvtShapeVars = cms.bool(True),
     leptonChannel = cms.string(options.lepton),
 
     muPtSrc = cms.InputTag("goodSignalMuonsNTupleProducer", "Pt"),
