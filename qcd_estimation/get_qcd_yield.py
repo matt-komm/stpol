7af044a5b31a5f72eaf3e301146116c0c566f068
Joosep Pata <joosep.pata@gmail.com>
2013-07-24 11:56:19 +0200

    revert

--- qcd_estimation/get_qcd_yield.py
+++ qcd_estimation/get_qcd_yield.py
@@ -18,8 +18,11 @@ from FitConfig import FitConfig
 from util_scripts import *
 from DataLumiStorage import *
 
-from plots.common.sample import load_samples
-import plots.common.cross_sections
+try:
+    from plots.common.cross_sections import lumi_iso, lumi_antiiso
+except Exception as e:
+    sys.stderr.write("You need to run `source $STPOL_DIR/setenv.sh` to use the custom python libraries\n")
+    raise e
 
 def get_yield(var, filename, cutMT, mtMinValue, fit_result, dataGroup):
     infile = "fits/"+var.shortName+"_fit_"+filename+".root"
@@ -58,7 +61,7 @@ def get_qcd_yield_with_fit(var, cuts, cutMT, mtMinValue, dataGroup, lumis, MCGro
     return (get_yield(var, cuts.name, cutMT, mtMinValue, fit, dataGroup), fit)
 
 
-def get_qcd_yield_with_selection(cuts, channel = "mu", base_path="$STPOL_DIR/step3_latest/", do_systematics=False):
+def get_qcd_yield_with_selection(cuts, channel = "mu", base_path="$STPOL_DIR/step3_new/", do_systematics=False):
 #    do_systematics = True
 #
 #    if channel == "ele":
@@ -90,10 +93,10 @@ def get_qcd_yield_with_selection(cuts, channel = "mu", base_path="$STPOL_DIR/ste
 
     if channel == "ele":
         cuts.setTrigger("1") #  || HLT_Ele27_WP80_v9==1 || HLT_Ele27_WP80_v8==1")
-        cuts.setIsolationCut("el_iso < 0.1")
-        cuts.setAntiIsolationCut("el_iso > 0.1 & el_iso < 0.5")
-        cuts.setAntiIsolationCutUp("el_iso > 0.11 & el_iso < 0.55") # check +-10% variation
-        cuts.setAntiIsolationCutDown("el_iso > 0.09 & el_iso < 0.45")
+        cuts.setIsolationCut("el_reliso < 0.1")
+        cuts.setAntiIsolationCut("el_reliso > 0.1 & el_reliso < 0.5")
+        cuts.setAntiIsolationCutUp("el_reliso > 0.11 & el_reliso < 0.55") # check +-10% variation
+        cuts.setAntiIsolationCutDown("el_reliso > 0.09 & el_reliso < 0.45")
         lepton_weight = "*electron_triggerWeight*electron_IDWeight"
     elif channel == "mu":
         lepton_weight = "*muon_TriggerWeight*muon_IsoWeight*muon_IDWeight"
@@ -105,8 +108,14 @@ def get_qcd_yield_with_selection(cuts, channel = "mu", base_path="$STPOL_DIR/ste
     #Luminosities for each different set of data have to be specified.
     #Now only for iso and anti-iso. In the future additional ones for systematics.
     #See DataLumiStorage for details if needed
-    dataLumiIso = plots.common.cross_sections.lumis["83a02e9_Jul22"]["iso"][channel]
-    dataLumiAntiIso = plots.common.cross_sections.lumis["83a02e9_Jul22"]["antiiso"][channel]
+    print cuts
+    if channel == "mu":
+        dataLumiIso = lumi_iso["mu"]
+        dataLumiAntiIso = lumi_antiiso["mu"]
+
+    if channel == "ele":
+        dataLumiIso = lumi_iso["ele"]
+        dataLumiAntiIso = lumi_antiiso["ele"]
 
     lumis = DataLumiStorage(dataLumiIso, dataLumiAntiIso)
 
@@ -134,16 +143,10 @@ def get_qcd_yield_with_selection(cuts, channel = "mu", base_path="$STPOL_DIR/ste
 
     #Generate path structure as base_path/iso/systematic, see util_scripts
     #If you have a different structure, change paths manually
-    #paths = generate_paths(systematics, base_path)
-
-    __paths = load_samples()
+    paths = generate_paths(systematics, base_path)
     #For example:
-    paths = dict()
-    paths["iso"] = dict()
-    paths["antiiso"] = dict()
-    paths["iso"]["Nominal"] = __paths["Jul15"]["mc"][channel]["nominal"]["iso"] + __paths["Jul15"]["data"][channel]["NONE"]["iso"]
-    paths["antiiso"]["Nominal"] = __paths["Jul15"]["mc"][channel]["nominal"]["antiiso"] + __paths["Jul15"]["data"][channel]["NONE"]["antiiso"]
-    
+    paths["iso"]["Nominal"] = base_path+"/iso/nominal/"
+    paths["antiiso"]["Nominal"] = base_path+"/antiiso/nominal/"
     #Then open files
     print "opening data and MC files"
     openedFiles = open_all_data_files(dataGroup, MCGroups, QCDGroup, paths)
@@ -180,7 +183,7 @@ if __name__=="__main__":
     cuts["3j1t"] = cuts_3j1t
     cuts["final_without_eta"] = cuts_final_without_eta
     cuts["mva"] = cuts_mva
-
+    
     #Remove the name of this script from the argument list in order to not confuse ArgumentParser
     try:
         sys.argv.pop(sys.argv.index("get_qcd_yield.py"))
