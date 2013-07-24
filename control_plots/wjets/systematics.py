95c5d855297cfe433acbbbef701a94f6964ac664
Joosep Pata <joosep.pata@gmail.com>
2013-07-25 01:34:13 +0200

    save hists

--- control_plots/wjets/systematics.py
+++ control_plots/wjets/systematics.py
@@ -1,18 +1,21 @@
 import ROOT
-from plot_utils import data_mc
+from plot_utils import data_mc, plot_hists_dict
 from plots.common.cuts import Cuts, Weights
 from plots.common.sample import Sample, get_paths
 from plots.common.cross_sections import lumi_iso
 from plots.common.histogram import HistCollection
-import logging
-import rootpy
+import rootpy, numpy
 import glob
 import re
+import root_numpy
+import pdb
+from copy import deepcopy
 
-logger = logging.getLogger("systematics")
 if __name__=="__main__":
+	import logging
 	rootpy.log.basic_config_colorized()
-	rootpy.log.setLevel(level=logging.WARNING)
+	logging.basicConfig(level=logging.WARNING)
+	logger = rootpy.log["/systematics"]
 
 	data_repro = "Jul15"
 	path = get_paths()[data_repro]["mc"]["mu"]["nominal"]["iso"]
@@ -23,6 +26,8 @@ if __name__=="__main__":
 	]
 	logger.info("samples %s" % samples)
 
+
+	cut = Cuts.final(2,1)
 	tot_hists = dict()
 	for wn, w in [
 		("nominal", Weights.wjets_madgraph_shape_weight("nominal")),
@@ -30,12 +35,17 @@ if __name__=="__main__":
 		("shape_down", Weights.wjets_madgraph_shape_weight("wjets_down"))
 	]:
 		p = data_mc(
-			"cos_theta", "2J1T SR",
-			Cuts.final(2,1), Weights.total("mu", "nominal")*w,
+			"cos_theta", "2J1T_%s" % (wn),
+			cut, Weights.total("mu", "nominal")*w,
 			samples, ".", True, lumi_iso["mu"], plot_range=[20, -1, 1],
 		)
+		sumw = [(sample.name, numpy.mean(root_numpy.root2array(sample.tfile.GetPath()[:-2], "trees/WJets_weights", branches=[str(w)])[str(w)])) for sample in samples]
 		s = sum(p.hists.values())
-		tot_hists[wn] = s
+		tot_hists[wn] = deepcopy(s)
 
 	hc = HistCollection(tot_hists, name="wjets")
+	for hn, h in hc.hists.items():
+		print hn, h.Integral()
+
+	canv = plot_hists_dict(hc.hists)
 	hc.save(".")
