1b4f19392669bfa158afec8579a95e991bc727d9
Mario Kadastik <mario@cern.ch>
2013-07-24 18:05:21 +0300

    Updated QCD scales

--- plots/common/plot_defs.py
+++ plots/common/plot_defs.py
@@ -16,15 +16,15 @@ cutlist['final_mu']=cutlist['nomt_mu']*Cuts.mt_mu
 
 qcdScale={}
 qcdScale['ele']={}
-qcdScale['ele']['final']=3.32
-qcdScale['ele']['nomet']=3.32
-qcdScale['ele']['presel']=4.05
-qcdScale['ele']['2j0t']=2.46
-qcdScale['ele']['3j1t']=0.28
+qcdScale['ele']['final']=0.83
+qcdScale['ele']['nomet']=1.33
+qcdScale['ele']['presel']=1.33
+qcdScale['ele']['2j0t']=2.25
+qcdScale['ele']['3j1t']=0.26
 qcdScale['mu']={}
-qcdScale['mu']['final']=3.32
-qcdScale['mu']['nomet']=3.32
-qcdScale['mu']['presel']=4.05
+qcdScale['mu']['final']=28
+qcdScale['mu']['nomet']=28
+qcdScale['mu']['presel']=27.9
 qcdScale['mu']['2j0t']=2.46
 qcdScale['mu']['3j1t']=0.28
 
