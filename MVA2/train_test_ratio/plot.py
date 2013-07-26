dc419d5193b66b0c92269427447082fb5eeb2637
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-07-26 18:59:02 +0200

    Fix ranges in traintest plot.py

--- MVA2/train_test_ratio/plot.py
+++ MVA2/train_test_ratio/plot.py
@@ -1,8 +1,8 @@
 import numpy as np
 import ROOT
 
-qs = np.arange(0.02, 1.00, 0.02)
-Ns = range(5, 105, 5)
+qs = np.linspace(0.02, .98, 49)
+Ns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 20, 22, 23, 25, 28, 30, 33, 35, 38, 42, 45, 49, 53, 58, 63, 68, 74, 81, 87, 95, 103, 112, 121, 132, 143, 155, 168, 183, 198, 215, 233, 253, 275, 298, 323, 351, 380, 413, 448, 486, 527, 572, 620, 673, 730, 792, 859, 932, 1011, 1097, 1190, 1291, 1401, 1519, 1648, 1788, 1940, 2104, 2283, 2477, 2687, 2915, 3162]
 
 KSSgraph = ROOT.TGraph2D(len(Ns)*len(qs)) # Kolmogorov-Smirnov for signal
 KSSgraph.SetNameTitle("KSSgraph", "Kolmogorov-Smirnov for signal")
