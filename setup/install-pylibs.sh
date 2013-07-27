8c6f5c22d08078a11278861227cbedce68053404
jpata <joosep.pata@gmail.com>
2013-07-27 16:39:42 +0300

    using pip

--- setup/install-pylibs.sh
+++ setup/install-pylibs.sh
@@ -20,35 +20,39 @@ wget --no-check-certificate https://pypi.python.org/packages/source/s/setuptools
 tar xf setuptools-0.9.5.tar.gz
 cd setuptools-0.9.5
 python setup.py install --prefix=$STPOL_DIR/local > /dev/null
-cd ..
+cd $STPOL_DIR/local
 python -c 'import setuptools;print "setuptools version=",setuptools.__version__'
 
-echo "Installing argparse"
-wget http://argparse.googlecode.com/files/argparse-1.2.1.tar.gz > /dev/null
-tar xf argparse-1.2.1.tar.gz
-cd argparse-1.2.1
+echo "Installing pip"
+wget --no-check-certificate https://pypi.python.org/packages/source/p/pip/pip-1.4.tar.gz
+tar xf pip-1.4.tar.gz 
 python setup.py install --prefix=$STPOL_DIR/local > /dev/null
-cd ..
+cd $STPOL_DIR/local
 
-python -c 'import argparse;print "Argparse version=",argparse.__version__'
 
-echo "Installing sqlalchemy"
-wget --no-check-certificate https://pypi.python.org/packages/source/S/SQLAlchemy/SQLAlchemy-0.8.1.tar.gz > /dev/null
-tar xf SQLAlchemy-0.8.1.tar.gz
-cd SQLAlchemy-0.8.1
-python setup.py install --prefix=$STPOL_DIR/local > /dev/null
-cd ..
+$STPOL_DIR/local/bin/pip install --install-option="--prefix=$STPOL_DIR/local" shortuuid
+$STPOL_DIR/local/bin/pip install --install-option="--prefix=$STPOL_DIR/local" argparse
+
+#echo "Installing argparse"
+#wget http://argparse.googlecode.com/files/argparse-1.2.1.tar.gz > /dev/null
+#tar xf argparse-1.2.1.tar.gz
+#cd argparse-1.2.1
+#python setup.py install --prefix=$STPOL_DIR/local > /dev/null
+#cd ..
+#
+#python -c 'import argparse;print "Argparse version=",argparse.__version__'
+#
+#echo "Installing sqlalchemy"
+#wget --no-check-certificate https://pypi.python.org/packages/source/S/SQLAlchemy/SQLAlchemy-0.8.1.tar.gz > /dev/null
+#tar xf SQLAlchemy-0.8.1.tar.gz
+#cd SQLAlchemy-0.8.1
+#python setup.py install --prefix=$STPOL_DIR/local > /dev/null
+#cd ..
+#
 
 echo "Installing rootpy"
 git clone git://github.com/rootpy/rootpy.git > /dev/null
 cd rootpy
 python setup.py install --prefix=$STPOL_DIR/local > /dev/null
 python -c 'import rootpy;print "rootpy version=",rootpy.__version__'
-cd ..
-
-echo "Installing shortuuid"
-git clone https://github.com/stochastic-technologies/shortuuid.git > /dev/null
-cd shortuuid
-python setup.py install --prefix=$STPOL_DIR/local > /dev/null
-python -c 'import shortuuid;print "shortuuid version=",shortuuid.__version__'
-cd ..
+cd $STPOL_DIR/local
