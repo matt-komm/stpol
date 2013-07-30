749716c6889eb594e6c57e7899b7daf0f1adbfa8
Ants Remm <ants.remm@cern.ch>
2013-07-30 20:11:12 +0200

    add plotting script that uses Joosep's code

---
+++ MVA2/BDT/04_jplot.py
@@ -0,0 +1,57 @@
+import ROOT
+from plots.common.data_mc import data_mc_plot
+from plots.common.sample import Sample
+from plots.common.cuts import Weights
+from plots.common.util import PhysicsProcess
+import MVA2.common
+
+lumis = {
+    "mu": 6784+6398+5277,
+    "ele":12410+6144
+}
+
+lepton_channel = "mu"
+lumi = lumis[lepton_channel]
+name = "antsu_plot :)"
+
+# plot definition
+plot_def={
+    'enabled': True,
+    'var': 'cos_theta',
+    'range': [20,-1,1],
+    'iso': True,
+    'estQcd': 'final_2j1t',
+    'gev': False,
+    'log': False,
+    'xlab': varnames["cos_theta"],
+    'labloc': 'top-left',
+    'elecut': cutlist['2j1t']*cutlist['presel_ele']*Cut('mva_BDT_all_mu_Mario>0.154'),
+    'mucut': cutlist['2j1t']*cutlist['presel_mu']*Cut('mva_BDT_all_mu_Mario>0.154')
+}
+
+weight = Weights.total(lepton_channel)*Weights.wjets_madgraph_shape_weight()*Weights.wjets_madgraph_flat_weight()
+physics_processes = PhysicsProcess.get_proc_dict(lepton_channel=lepton_channel)#Contains the information about merging samples and proper pretty names for samples
+merge_cmds = PhysicsProcess.get_merge_dict(physics_processes) #The actual merge dictionary
+
+#Get the file lists
+flist = get_file_list(
+	merge_cmds,
+	"filled_trees_mu/"
+)
+if len(flist)==0:
+	raise Exception("Couldn't open any files. Are you sure that %s exists and contains root files?" % args.indir)
+
+samples={}
+for f in flist:
+	samples[f] = Sample.fromFile(f, tree_name=tree)
+
+
+canv, merged_hists, htot_mc, htot_data = data_mc_plot(samples, plot_def, name, lepton_channel, lumi, weight, merge_cmds)
+
+img = ROOT.TImage.Create()
+img.FromPad(canv)
+img.WriteImage(name+".png")
+
+
+
+
