3fec87898d5f47b49947350f36c9cf76f8019091
Joosep Pata <joosep.pata@gmail.com>
2013-07-30 22:52:03 +0200

    preliminary setup code

--- CMSSW_5_3_11/src/SingleTopPolarization/BTagEfficiencyAnalyzer/src/BTagEfficiencyAnalyzer.cc
+++ CMSSW_5_3_11/src/SingleTopPolarization/BTagEfficiencyAnalyzer/src/BTagEfficiencyAnalyzer.cc
@@ -2,13 +2,13 @@
 //
 // Package:    BTagEfficiencyAnalyzer
 // Class:      BTagEfficiencyAnalyzer
-// 
+//
 /**\class BTagEfficiencyAnalyzer BTagEfficiencyAnalyzer.cc SingleTopPolarization/BTagEfficiencyAnalyzer/src/BTagEfficiencyAnalyzer.cc
 
- Description: [one line class summary]
+ Description: This class calculates the probabilities for b-tagging jets of various flavour, as dependent on the pt and eta of the jet.
 
  Implementation:
-     [Notes on implementation]
+     The output is saved in a TFile as TH2F histograms.
 */
 //
 // Original Author:  Joosep Pata
@@ -29,128 +29,161 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
-// class declaration
-//
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-class BTagEfficiencyAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit BTagEfficiencyAnalyzer(const edm::ParameterSet&);
-      ~BTagEfficiencyAnalyzer();
+const float eps = std::numeric_limits<float>::epsilon();
+bool AreSame(float a, float b)
+{
+    return std::fabs(a - b) < eps;
+}
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-
-   private:
-      virtual void beginJob() ;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
-
-      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
-      virtual void endRun(edm::Run const&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-
-      // ----------member data ---------------------------
+//Binnings taken as the ones used for the scale factors by the b-tagging POG
+static const unsigned int n_eta_bins = 4;
+const Double_t eta_bins_low[n_eta_bins] =
+{
+    (Double_t)0.0,
+    (Double_t)0.8,
+    (Double_t)1.6,
+    (Double_t)2.4
 };
 
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-//
-BTagEfficiencyAnalyzer::BTagEfficiencyAnalyzer(const edm::ParameterSet& iConfig)
-
+static const unsigned int n_pt_bins = 18;
+const Double_t pt_bins_low[n_pt_bins] =
 {
-   //now do what ever initialization is needed
+    0,
+    20,
+    30,
+    40,
+    50,
+    60,
+    70,
+    80,
+    100,
+    120,
+    160,
+    210,
+    260,
+    320,
+    400,
+    500,
+    600,
+    800
+};
 
+class BTagEfficiencyAnalyzer : public edm::EDAnalyzer
+{
+public:
+    explicit BTagEfficiencyAnalyzer(const edm::ParameterSet &);
+    ~BTagEfficiencyAnalyzer();
+
+    static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+
+
+private:
+    virtual void beginJob() ;
+    virtual void analyze(const edm::Event &, const edm::EventSetup &);
+    virtual void endJob() ;
+
+    virtual void beginRun(edm::Run const &, edm::EventSetup const &);
+    virtual void endRun(edm::Run const &, edm::EventSetup const &);
+    virtual void beginLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &);
+    virtual void endLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &);
+
+    const edm::InputTag jetPtSrc;
+    const edm::InputTag jetEtaSrc;
+    const edm::InputTag jetFlavourSrc;
+    const edm::InputTag jetBDiscriminatorSrc;
+    const edm::InputTag nJetSrc;
+    const float bDiscriminatorWP;
+};
+
+BTagEfficiencyAnalyzer::BTagEfficiencyAnalyzer(const edm::ParameterSet &iConfig)
+    : jetPtSrc(iConfig.getParameter<edm::InputTag>("jetPtSrc"))
+    , jetEtaSrc(iConfig.getParameter<edm::InputTag>("jetEtaSrc"))
+    , jetFlavourSrc(iConfig.getParameter<edm::InputTag>("jetFlavourSrc"))
+    , jetBDiscriminatorSrc(iConfig.getParameter<edm::InputTag>("jetBDiscriminatorSrc"))
+    , nJetSrc(iConfig.getParameter<edm::InputTag>("nJetSrc"))
+    , bDiscriminatorWP(iConfig.getParameter<float>("bDiscriminatorWP"))
+{
+    std::map<int, TDirectory *> dirs;
+    const std::vector<const char *> flavours;
+    flavours.push_back("b");
+    flavours.push_back("c");
+    flavours.push_back("l");
+
+    const Double_t *p_pt_bins_low = (const Double_t *)&pt_bins_low;
+    const Double_t *p_eta_bins_low = (const Double_t *)&eta_bins_low;
+    Int_t _n_pt_bins = (Int_t)n_pt_bins - 1;
+    Int_t _n_eta_bins = (Int_t)n_eta_bins - 1;
+
+    for (int i = 2; i <= 3; i++)
+    {
+        std::stringstream ss;
+        ss << i << "J";
+        dirs[i] = fs->make<TDirectory>(ss.string().c_str());
+        for (auto & f : flavours)
+        {
+            std::stringstream ss;
+            ss << "eff_" << f;
+            const std::string hn = ss.string();
+            dirs[i]->cd();
+            fs->make<TH2F>(f.c_str(), f.c_str(), _n_pt_bins, p_pt_bins_low, _n_eta_bins, p_eta_bins_low);
+        }
+    }
 }
 
 
 BTagEfficiencyAnalyzer::~BTagEfficiencyAnalyzer()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
 }
 
 
-//
-// member functions
-//
-
-// ------------ method called for each event  ------------
 void
-BTagEfficiencyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+BTagEfficiencyAnalyzer::analyze(const edm::Event &iEvent, const edm::EventSetup &iSetup)
 {
-   using namespace edm;
-
-
-
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+    using namespace edm;
 }
 
 
-// ------------ method called once each job just before starting event loop  ------------
-void 
+void
 BTagEfficiencyAnalyzer::beginJob()
 {
 }
 
-// ------------ method called once each job just after ending the event loop  ------------
-void 
-BTagEfficiencyAnalyzer::endJob() 
+void
+BTagEfficiencyAnalyzer::endJob()
 {
 }
 
-// ------------ method called when starting to processes a run  ------------
-void 
-BTagEfficiencyAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
+void
+BTagEfficiencyAnalyzer::beginRun(edm::Run const &, edm::EventSetup const &)
 {
 }
 
-// ------------ method called when ending the processing of a run  ------------
-void 
-BTagEfficiencyAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+void
+BTagEfficiencyAnalyzer::endRun(edm::Run const &, edm::EventSetup const &)
 {
 }
 
-// ------------ method called when starting to processes a luminosity block  ------------
-void 
-BTagEfficiencyAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+void
+BTagEfficiencyAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &)
 {
 }
 
 // ------------ method called when ending the processing of a luminosity block  ------------
-void 
-BTagEfficiencyAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+void
+BTagEfficiencyAnalyzer::endLuminosityBlock(edm::LuminosityBlock const &, edm::EventSetup const &)
 {
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
-BTagEfficiencyAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
+BTagEfficiencyAnalyzer::fillDescriptions(edm::ConfigurationDescriptions &descriptions)
+{
+    edm::ParameterSetDescription desc;
+    desc.setUnknown();
+    descriptions.addDefault(desc);
 }
 
-//define this as a plug-in
-DEFINE_FWK_MODULE(BTagEfficiencyAnalyzer);
+v.begin(), v.end()DEFINE_FWK_MODULE(BTagEfficiencyAnalyzer);
