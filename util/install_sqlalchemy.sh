6664c8de01c791e7003213b5efe94b51b54385d9
jpata <joosep.pata@gmail.com>
2013-07-31 23:21:20 +0300

    new PU histos

--- util/install_sqlalchemy.sh
+++
@@ -1,6 +0,0 @@
-cd $STPOL_DIR
-wget --no-check-certificate https://pypi.python.org/packages/source/S/SQLAlchemy/SQLAlchemy-0.8.1.tar.gz
-tar xf SQLAlchemy-0.8.1.tar.gz
-mkdir local
-cd SQLAlchemy-0.8.1
-python setup.py install --prefix=$STPOL_DIR/local
