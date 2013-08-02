de07d04c4fde4cdb0e6ce60db432c360e6988298
jpata <joosep.pata@gmail.com>
2013-08-02 15:59:57 +0300

    fixed task file creation

--- analysis_step3/slurm_sub_step3.sh
+++ analysis_step3/slurm_sub_step3.sh
@@ -36,12 +36,9 @@ do
     echo "Submitting step3 job $CONF on file $file"
 
 #save the task
-    CMD="sbatch -p cms,phys,ied,prio $STPOL_DIR/analysis_step3/run_step3_eventloop.sh `readlink -f $file` $OUTDIR $CONF > task_$file"
-
+    CMD="sbatch -p prio,main $STPOL_DIR/analysis_step3/run_step3_eventloop.sh `readlink -f $file` $OUTDIR $CONF"
+    echo $CMD > task_$file
+    
 #try to submit until successfully submitted
-    until `eval $CMD`
-    do 
-        echo "ERROR!: could not submit slurm job on file $file, retrying after sleep..." >&2
-        sleep 0.5
-    done 
+    eval $CMD
 done
