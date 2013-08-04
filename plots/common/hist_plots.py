2ebbb789c57e8d4159c4d1aea23587fbafcb66e9
Joosep Pata <joosep.pata@gmail.com>
2013-08-04 18:14:31 +0200

    implemented plotting the systematic

--- plots/common/hist_plots.py
+++ plots/common/hist_plots.py
@@ -4,20 +4,41 @@ from utils import get_max_bin
 import math
 from plots.common.legend import legend
 from plots.common.histogram import norm
-
+import logging
+logger = logging.getLogger("hist_plots")
 
 def plot_hists(hists, name="canv", **kwargs):
+    """
+    Draws a list of histograms side-by-side on a new canvas.
+
+    Args:
+        hists - a list of Hist instances to draw
+
+    Keywords:
+        name: the name of the canvas to create.
+        x_label: a string with the title for the x axis.
+        y_label: ditto for the y axis.
+        do_log_y: a boolean to specify if the y axis should be logarithmic.
+        min_bin: a numerical value for the lower range of the y axis.
+        max_bin: ditto for the upper range. Overrides the max_bin_mult variable.
+        max_bin_mult: a multiplier for the y axis, applied on the maximal bin
+            height.
+        do_chi2: a boolean to specify whether to do the Chi2 test between
+            the first and subsequent histograms.
+        do_ks: ditto for the KS test.
+
+    Returns:
+        a handle to the new canvas.
+    """
+
     canv = ROOT.TCanvas(name, name)
     draw_cmd = kwargs["draw_cmd"] if "draw_cmd" in kwargs.keys() else "E1"
-#    title = kwargs["title"] if "title" in kwargs.keys() else "NOTITLE"
-    line_width = kwargs["line_width"] if "line_width" in kwargs.keys() else 2
     x_label = kwargs.get("x_label", "XLABEL")
     y_label = kwargs.get("y_label", "")
     do_log_y = kwargs["do_log_y"] if "do_log_y" in kwargs.keys() else False
     min_bin = kwargs.get("min_bin", 0)
     max_bin = kwargs.get("max_bin", None)
     max_bin_mult = kwargs.get("max_bin_mult", 1.5)
-    styles = kwargs.get("styles", {})
     do_chi2 = kwargs.get("do_chi2", False)
     do_ks = kwargs.get("do_ks", False)
 
@@ -28,7 +49,6 @@ def plot_hists(hists, name="canv", **kwargs):
         hist.Draw(draw_cmd + (" SAME" if first else ""))
         first = True
 
-#    hists[0].SetTitle(title)
     hists[0].SetStats(False)
     if not max_bin:
         hists[0].SetMaximum(max_bin_mult*max_bin_val)
@@ -54,12 +74,48 @@ def plot_hists(hists, name="canv", **kwargs):
 
     return canv
 
-def plot_data_mc_ratio(canv, hist_data, hist_mc, height=0.3):
+def subpad(low=0.3):
     """
-    Puts the data/MC ratio plot on the TCanvas canv. A new TPad is created at the bottom with the specified height.
-    canv - TCanvas
-    hist_data  
-    returns - (pad, histogram) where pad is the new TPad and histogram is the TH1F ratio histogram
+    Makes a new subpad on a TCanvas in the upper region
+
+    Args:
+        low: a floating point fraction for the area to cut off in the lower part
+
+    Returns:
+        a handle to the new TPad
+    """
+    #Make a separate pad for the stack plot
+    p1 = ROOT.TPad("p1", "p1", 0, low, 1, 1)
+    p1.Draw()
+    p1.SetTicks(1, 1);
+    p1.SetGrid();
+    p1.SetFillStyle(0);
+    p1.cd()
+    return subpad
+
+def plot_data_mc_ratio(canv, hist_data, hist_mc, height=0.3, syst_hists=None):
+    """
+    Puts the data/MC ratio plot on the TCanvas canv. A new TPad is created at
+    the bottom with the specified height.
+
+    Args:
+        canv: a TCanvas instance on which to draw the ratio plot. A subpad will
+            be created on this canvas
+        hist_data: A Hist instance with the data
+        hist_mc: A Hist instance with the MC
+
+    Keywords:
+        height: the floating value fraction of the height of the new pad
+        syst_hists: a 2-tuple (syst_up, syst_down) with Hist instances to
+            draw as the systematic error band
+
+    Returns:  
+        A tuple with (pad, histogram) where pad is the new TPad and histogram is
+        the TH1F ratio histogram that were created
+
+    Raises:
+        ValueError: when the format of the supplied systematic histograms was
+            not a 2-tuple
     """
     canv.cd()
     p2 = ROOT.TPad("p2", "p2", 0, 0, 1, height)
@@ -81,7 +137,6 @@ def plot_data_mc_ratio(canv, hist_data, hist_mc, height=0.3):
     hist_ratio.Divide(hist_data)
 
     hist_ratio.SetLineColor(ROOT.kBlack)
-    hist_ratio.SetMarkerColor(ROOT.kBlack)
 
     hist_ratio.SetStats(False)
     hist_ratio.SetMarkerStyle(20)
@@ -105,18 +160,47 @@ def plot_data_mc_ratio(canv, hist_data, hist_mc, height=0.3):
     yAxis.SetNdivisions(405)
 
     hist_ratio.Draw("p0e1")
+    logger.debug(list(hist_ratio.y()))
 
     hist_line = ROOT.TH1F("0line","0line",hist_mc.GetNbinsX(),hist_mc.GetBinLowEdge(1),hist_mc.GetBinLowEdge(hist_mc.GetNbinsX()+1))
     hist_line.Draw("lsame")
+    canv.hist_line = hist_line
 
-    return p2, hist_ratio, hist_line
+    if syst_hists:
+        logger.info("Drawing systematic histograms")
+        syst_ratio_hists = []
+        if len(syst_hists) != 2:
+            raise Exception("Must specify the systematic histograms as a 2-tuple, got %s" % str(syst_hists))
+        for h in list(syst_hists):
+            hr = h.Clone()
+            hr.Add(hist_data, -1)
+            hr.Divide(hist_data)
+
+            hr.fillstyle = "/"
+            hr.SetFillColor(ROOT.kGray)
+            hr.SetLineColor(ROOT.kGray)
+
+            hr.Draw("hist same")
+            logger.debug(list(hr.y()))
+            syst_ratio_hists.append(hr)
+        canv.syst_ratio_hists = syst_ratio_hists
+    return p2, hist_ratio
 
 
 def plot_hists_dict(hist_dict, setNames=True, **kwargs):
     """
-    Draws the contents of a dictionary of hists.
-    hist_dict - dict{str, Hist}: a collection of histograms to be drawn.
-    setNames - bool: The keys of the dict will be used as titles in the legend. Styling will be done automatically.
+    Draws the contents of a dictionary of hists. Styling will be done
+    automatically. Any additional kwargs are passed to the plot_hists and legend
+    methods.
+
+    Args:
+        hist_dict: a str, Hist dictionary of the histograms to be drawn.
+    
+    Keywords:
+        setNames: a bool to specify whether the keys of the dict will be used as
+            titles in the legend.
+
+    Returns: a handle to the new canvas
     """
     items = hist_dict.items()
     for hn, h in items:
@@ -131,3 +215,38 @@ def plot_hists_dict(hist_dict, setNames=True, **kwargs):
     leg = legend(hists, styles=["f", "f"], **kwargs)
     canv.LEGEND = leg
     return canv
+
+if __name__=="__main__":
+    logger.setLevel(logging.DEBUG)
+    ROOT.gROOT.SetBatch(True)
+    ROOT.TH1F.SetDefaultSumw2()
+    from plots.common.tdrstyle import tdrstyle
+    tdrstyle()
+    from rootpy.plotting import Hist
+    import numpy.random as nr
+
+    hdata = Hist(20, -1, 1, name="mc")
+    hmc = Hist(20, -1, 1, name="data")
+
+    for i in range(10000):
+        hdata.Fill(nr.normal(0, 1))
+    for i in range(100000):
+        hmc.Fill(nr.normal(0.05, 1))
+    hmc.Scale(1.05 * hdata.Integral()/ hmc.Integral())
+
+    c = ROOT.TCanvas("c1")
+
+    p1 = subpad()
+
+    hdata.Draw("h")
+    hmc.Draw("hsame")
+
+    hsyst_up = hmc.Clone()
+    hsyst_up.Scale(1.02)
+    hsyst_down = hmc.Clone()
+    hsyst_down.Scale(0.98)
+
+    p2, hist_ratio = plot_data_mc_ratio(c, hmc, hdata, syst_hists=(hsyst_up, hsyst_down))
+    c.Update()
+    c.Show()
+    c.SaveAs("hist_plots_test1.png")
