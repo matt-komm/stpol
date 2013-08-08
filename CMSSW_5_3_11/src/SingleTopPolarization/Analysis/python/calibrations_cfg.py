fdc88a4ff99dbba6c9e729f924a7870ee2001220
jpata <joosep.pata@gmail.com>
2013-08-08 16:10:36 +0300

    added wjets exclusive fastsim sample

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/calibrations_cfg.py
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/calibrations_cfg.py
@@ -111,7 +111,7 @@ def getEffFiles(channel):
     """
     if channel in ["T_t", "Tbar_t"] or re.match("T_t_.*", channel) or re.match("Tbar_t_.*", channel) or re.match("TToB.*Nu_.*", channel):
         return fnames["T_t"], fnames["WJets"], fnames["T_t"]
-    elif re.match("W[0-9]*Jets_exclusive", channel) or re.match("WJets.*", channel) or channel in ["WW", "WZ", "ZZ", "DYJets"] or channel.startswith("GJets") or channel.startswith("QCD"):
+    elif re.match("W[0-9]*Jets", channel) or re.match("WJets.*", channel) or channel in ["WW", "WZ", "ZZ", "DYJets"] or channel.startswith("GJets") or channel.startswith("QCD"):
         return fnames["T_t"], fnames["WJets"], fnames["WJets"]
     elif channel in ["TTbar", "T_tW", "T_s", "Tbar_tW", "Tbar_s"] or channel.startswith("TTJets"):
         return fnames["T_t"], fnames["WJets"], fnames["TTbar"]
