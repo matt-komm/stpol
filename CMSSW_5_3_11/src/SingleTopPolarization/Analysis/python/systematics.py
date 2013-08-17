7e9ed734cbf4ddd60dceb98ba1ef57b60a04b4b9
Joosep Pata <joosep.pata@gmail.com>
2013-08-17 18:35:53 +0200

    added options for symmetrizing errors, disabling stat

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/systematics.py
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/systematics.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import logging
 logger = logging.getLogger("systematics")
-
+logger.setLevel(logging.DEBUG)
 def to_arr(x):
     return np.array(list(x))
 
@@ -19,15 +19,15 @@ def n_events(hist):
 
 def cdf_errors(hist, sign=+1):
     n = n_events(hist)
-    logger.info("Errs: %s" % str())
+    #logger.info("Errs: %s" % str(to_arr(hist.errors())))
     for i in range(hist.nbins()):
-        hist.SetBinError(i,
+        hist.SetBinError(i+1,
             sign*0.5*math.sqrt(
                 n[i] + 0.25
             )
         )
 
-def total_syst(nominal, systs):
+def total_syst(nominal, systs, **kwargs):
     """
     Given a nominal histogram and a list of up/down variations
     corresponding to systematics, calculate the total up/down
@@ -41,6 +41,17 @@ def total_syst(nominal, systs):
             to the model
         systs: a list of tuples (name, (hist_up, hist_down))
             corresponding to a systematic variation.
+
+    Keywords:
+        consider_variated_stat_err:
+            since the variated templates are also derived from
+            limited statistics, one must, in principle,
+            consider their means as a random variable drawn
+            from a distribution.
+        symmetric: symmetrize the systematic errors, using a
+            Gaussian with mean=nominal and sigma=up-down.
+            Otherwise, a two-sided gaussian is assumed, with
+            mean=nominal, sigma1=up-nominal, sigma2=nominal-down.
     Returns:
         a 4-tuple of histograms corresponding to the total uncorrelated
         systematic up/down variation and the uncorrelated
@@ -48,6 +59,18 @@ def total_syst(nominal, systs):
     """
     if isinstance(systs, dict):
         systs = systs.items()
+
+    consider_variated_stat_err = kwargs.get("consider_variated_stat_err", True)
+
+    if consider_variated_stat_err:
+        consider_variated_stat_err = 1.0
+    else:
+        consider_variated_stat_err = 0.0
+        logger.debug(
+            """Not taking into account the
+            statistical error in the variated templates"""
+        )
+    symmetric = kwargs.get("symmetric", True)
 
     bins = to_arr(nominal.y())
     errs = to_arr(nominal.errors())
@@ -70,11 +93,15 @@ def total_syst(nominal, systs):
 
         # The difference between the nominal and the variated shape, taking into account
         # the lack of information about the central value of the variated shapes from low statistics.
-        diff_up = quadsum(bins - bins_up, to_arr(hup.errors()))
-        diff_down = quadsum(bins - bins_down, to_arr(hdown.errors()))
+        diff_up = quadsum(bins - bins_up,
+            float(consider_variated_stat_err)*to_arr(hup.errors())
+        )
+        diff_down = quadsum(bins - bins_down,
+            float(consider_variated_stat_err)*to_arr(hdown.errors())
+        )
         diff = quadsum(bins_up - bins_down,
-            to_arr(hdown.errors()),
-            to_arr(hup.errors())
+            float(consider_variated_stat_err)*-to_arr(hdown.errors()),
+            float(consider_variated_stat_err)*to_arr(hup.errors())
         )
 
         diff_up_tot += np.power(diff_up, 2)
@@ -85,24 +112,34 @@ def total_syst(nominal, systs):
     diff_down_tot = np.sqrt(diff_down_tot)
     diff_tot = np.sqrt(diff_tot)
 
-    #Systematic only
+    #Systematic variation only
     hup = nominal.Clone("syst_up")
     hdown = nominal.Clone("syst_down")
-    bins_up = bins + diff_up_tot
-    bins_down = bins - diff_down_tot
+    if symmetric:
+        bins_up = bins + diff_tot
+        bins_down = bins - diff_tot
+    else:
+        bins_up = bins + diff_up_tot
+        bins_down = bins - diff_down_tot
+
     for i in range(len(bins)):
         hup.SetBinContent(i+1, bins_up[i])
         hdown.SetBinContent(i+1, bins_down[i])
 
-    #Statistical plus systematic, uncorrelated
+    #Statistical plus systematic, assuming uncorrelated
     hup_syst_stat = nominal.Clone("syst_stat_up")
     hdown_syst_stat = nominal.Clone("syst_stat_down")
     delta_up = quadsum(diff_up_tot, errs)
     delta_down = quadsum(diff_down_tot, errs)
     delta = quadsum(diff_tot, errs)
 
-    bins_up = bins + delta_up
-    bins_down = bins - delta_down
+    if symmetric:
+        bins_up = bins + delta
+        bins_down = bins - delta
+    else:
+        bins_up = bins + delta_up
+        bins_down = bins - delta_down
+
     for i in range(len(bins)):
         hup_syst_stat.SetBinContent(i+1, bins_up[i])
         hdown_syst_stat.SetBinContent(i+1, bins_down[i])
