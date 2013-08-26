885d3622d59bee543e4bdde96bff29959d0c9af0
Joosep Pata <joosep.pata@gmail.com>
2013-08-26 12:55:11 +0200

    reformat

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/cuts_base.h
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/cuts_base.h
@@ -11,10 +11,12 @@
 
 //Shorthand for getting a value of type T from the event
 template <typename T>
-T get_collection(const edm::EventBase& evt, edm::InputTag src, T retval) {
+T get_collection(const edm::EventBase &evt, edm::InputTag src, T retval)
+{
     edm::Handle<T> coll;
     evt.getByLabel(src, coll);
-    if(!coll.isValid()) {
+    if (!coll.isValid())
+    {
 #ifdef DEBUG
         std::cerr << "Collection " << src.encode() << " is not valid!" << std::endl;
 #endif
@@ -28,52 +30,56 @@ T get_collection(const edm::EventBase& evt, edm::InputTag src, T retval) {
     return *coll;
 }
 
-class BranchVars {
+class BranchVars
+{
 public:
     std::map<std::string, int> vars_int;
     std::map<std::string, float> vars_float;
     std::map<std::string, std::vector<float>> vars_vfloat;
-    
+
     //the default values for TTree entries
     static const float def_val;
     static const int def_val_int;
-    
+
 };
 
 //Shorthand for getting a value of type T from the event, where the original container is vector<T>
 template <typename T>
-T get_collection_n(const edm::EventBase& evt, edm::InputTag src, unsigned int n=0) {
+T get_collection_n(const edm::EventBase &evt, edm::InputTag src, unsigned int n = 0)
+{
     edm::Handle<std::vector<T>> coll;
     evt.getByLabel(src, coll);
-    if(!(coll.isValid()) || n >= coll->size()) {
+    if (!(coll.isValid()) || n >= coll->size())
+    {
         return BranchVars::def_val;
     }
     return (T)(coll->at(n));
 }
 
 //Base class for all work that is done inside the loop
-class CutsBase {
+class CutsBase
+{
 public:
-    
+
     //Map of the branch variables
-    BranchVars& branch_vars;
-    
+    BranchVars &branch_vars;
+
     //Counter for the number of processed events
     unsigned long n_processed;
-    
+
     //Counter for the number of events passing this Cut
     unsigned long n_pass;
-    
+
     //Abstract method that sets the branch variables to sensible defaults on each loop
     virtual void initialize_branches() = 0;
-    
+
     //Actually processes the event, loading the variables form the edm::EventBase into the branches
-    virtual bool process(const edm::EventBase& event) = 0;
-    
+    virtual bool process(const edm::EventBase &event) = 0;
+
     std::string toString();
-    
-    CutsBase(BranchVars& _branch_vars);
-    
+
+    CutsBase(BranchVars &_branch_vars);
+
     void pre_process();
     void post_process();
 };
