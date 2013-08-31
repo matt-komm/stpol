58296856359529c7b04ec613a9cebe5824e03e15
Joosep Pata <joosep.pata@gmail.com>
2013-08-31 16:12:38 +0300

    fixed relative paths

--- src/batch_step2/run.sh
+++ src/batch_step2/run.sh
@@ -2,5 +2,5 @@
 
 for fi in `find $STPOL_DIR/filelists/Jul10_5a56de/step1/mc/wjets_FSIM_Summer12/ -name "*.txt"`
 do
-    ./step2.sh $fi
+    $STPOL_DIR/src/batch_step2/step2.sh $fi
 done
--- src/batch_step2/step2.sh
+++ src/batch_step2/step2.sh
@@ -2,14 +2,14 @@
 CFG=$STPOL_DIR/CMSSW_5_3_11/src/SingleTopPolarization/Analysis/python/runconfs/step2/step2.py 
 JOBNAME=`basename $1`
 JOBNAME="${JOBNAME%.*}"
-
+JOBNAME="WD_"$JOBNAME
 mkdir $JOBNAME
 COUNTER=0
 
-for line in `cat $1 | ./chunk.py`
+for line in `cat $1 | $STPOL_DIR/src/batch_step2/chunk.py`
 do
     cd $JOBNAME
-    echo sbatch ../cmsRun.sh $CFG isMC=True subChannel=WJets inputFiles=$line > job_$COUNTER
+    echo sbatch $STPOL_DIR/src/batch_ste2/cmsRun.sh $CFG isMC=True subChannel=WJets inputFiles=$line > job_$COUNTER
     cd ..
 
     COUNTER=$((COUNTER + 1))
