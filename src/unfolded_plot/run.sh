e7f365b8b0b44b5f9e7ce77ca91c915d98b95a49
Joosep Pata <joosep.pata@gmail.com>
2013-09-03 21:01:25 +0200

    new histogram path from SVN

--- src/unfolded_plot/run.sh
+++ src/unfolded_plot/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python $STPOL_DIR/src/unfolded_plot/pseudoexp.py --channel=mu results/unfolding/Aug28/muon/unfolded.root results/unfolding/Aug28/muon/unfolded_data.root
-python $STPOL_DIR/src/unfolded_plot/pseudoexp.py --channel=ele results/unfolding/Aug28/electron/unfolded.root results/unfolding/Aug28/electron/unfolded_data.root
+python $STPOL_DIR/src/unfolded_plot/pseudoexp.py --channel=mu results/unfolding/Aug28/muon/unfolded.root /Users/joosep/Documents/stpol/notes/analysis_notes/notes/AN-12-448/trunk/results/muon/unfolded.root
+python $STPOL_DIR/src/unfolded_plot/pseudoexp.py --channel=ele results/unfolding/Aug28/electron/unfolded.root /Users/joosep/Documents/stpol/notes/analysis_notes/notes/AN-12-448/trunk/results/electron/unfolded.root
