b5e0f686b61fa07e55351d0308a369ee66941e87
Joosep Pata <joosep.pata@gmail.com>
2013-09-06 11:41:15 +0300

    hadd in parallel

--- util/haddDirs.sh
+++ util/haddDirs.sh
@@ -2,11 +2,12 @@
 #This script will take the out_step3 folder and generate merged the trees from all the subdirectories
 #Use as haddDirs.sh /path/to/out_step3/
 
-files=`find $1 -name "*.root" -exec dirname {} \; | sort | uniq`
+find $1 -name "*.root" -exec dirname {} \; | sort | uniq | $STPOL_DIR/local/bin/parallel hadd -O -f9 {}.root {}/out*.root 
 
-for f in $files
-do
-    if [[ ! -f $f.root ]]; then
-        hadd -O -f9 $f.root $f/out*.root
-    fi
-done
+
+#for f in $files
+#do
+#    if [[ ! -f $f.root ]]; then
+#        hadd -O -f9 $f.root $f/out*.root
+#    fi
+#done
