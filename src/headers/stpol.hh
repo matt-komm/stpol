8583e71876740196054ef8d3b2e2875d7c2a262e
Joosep Pata <joosep.pata@gmail.com>
2013-10-01 14:03:46 +0300

    use a class with public statics instead of a namespace

--- src/headers/stpol.hh
+++ src/headers/stpol.hh
@@ -1,4 +1,5 @@
 //We need to have a missing value identifier. NaN is a good candidate but not ideal.
+#include "ROOT.jl/src/fwlite.hh"
 static const double NA = nan("");
 
 static const char *PROCESS = "STPOLSEL2";
@@ -13,21 +14,42 @@ float value(const std::vector<float> *p, unsigned int n = 0)
     return p->at(n);
 }
 
+class SimpleGetter {
+    private:
+        h_vfloat handle;
+        const label lab;
+    public:
+        SimpleGetter(const char* label, const char* instance, const char* process)
+        : handle()
+        , lab(make_tuple(label, instance, process))
+        {
+            
+        }
+
+        const std::vector<float>* get(fw_event *ev) {
+            return get_vfloat(&handle, ev, lab);
+        }
+};
 
 //It is convenient to group objects to namespaces
 //Instead of copying this for electrons etc, one should generalize!
-namespace mu
+class mu
 {
 
-static const char *mu_src = "goodSignalMuonsNTupleProducer";
+private:
+static const char *src;
+static SimpleGetter _pt;
+static SimpleGetter _eta;
+static SimpleGetter _phi;
 
-const label pt = make_tuple(mu_src, "Pt", PROCESS);
-h_vfloat *h_pt = new h_vfloat();
-
-double get_pt(fw_event *ev)
+public:
+const static float pt(fw_event *ev)
 {
-    const std::vector<float> *p = get_vfloat(h_pt, ev, pt);
-    return value(p);
+    return value(_pt.get(ev));
 }
 };
 
+const char* mu::src = "goodSignalMuonsNTupleProducer";
+SimpleGetter mu::_pt(mu::src, "Pt", PROCESS);
+SimpleGetter mu::_eta(mu::src, "Eta", PROCESS);
+SimpleGetter mu::_phi(mu::src, "Phi", PROCESS);
