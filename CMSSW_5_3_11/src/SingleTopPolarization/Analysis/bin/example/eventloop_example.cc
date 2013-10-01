ceb00c34371b0c035f05b341f4e65f27799a673e
Joosep Pata <joosep.pata@gmail.com>
2013-10-01 15:47:09 +0300

    better interface

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/example/eventloop_example.cc
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/example/eventloop_example.cc
@@ -1,5 +1,9 @@
 //This file can be copied as an example for your own loop
+
+//The public interface, which is kept more or less constant
 #include "src/headers/stpol.hh" //present in $STPOL_DIR/src/headers/stpol.hh
+
+using namespace stpol::stable;
 
 int main(int argc, char **argv)
 {
@@ -16,9 +20,13 @@ int main(int argc, char **argv)
     for (unsigned int i = 0; i < events->size(); i++)
     {
         events->to(i);
+        
+        const event_id evid = get_event_id(events); //defined in fwlite.hh
 
-        float pt = mu::pt(events); //defined in stpol.hh
-        std::cout << pt << std::endl;
+        float mu_pt = signal::muon.pt(events); //defined in stpol.hh, namespace stpol::stable
+        float ele_pt = signal::electron.pt(events);
+        
+        std::cout << evid.run << ":" << evid.lumi << ":" << evid.event << " " << mu_pt << " | " << ele_pt << std::endl;
     }
 
     return 0;
--- src/headers/stpol.hh
+++ src/headers/stpol.hh
@@ -1,5 +1,10 @@
 //We need to have a missing value identifier. NaN is a good candidate but not ideal.
-#include "ROOT.jl/src/fwlite.hh"
+#include "ROOT.jl/src/fwlite.hh" //In $STPOL_DIR/deps
+
+namespace stpol {
+
+//A private interface that may change    
+namespace unstable {
 static const double NA = nan("");
 
 static const char *PROCESS = "STPOLSEL2";
@@ -14,12 +19,15 @@ float value(const std::vector<float> *p, unsigned int n = 0)
     return p->at(n);
 }
 
-class SimpleGetter {
+
+//A simple Handle<vector<float>> wrapper
+class SimpleGetterVFloat {
     private:
-        h_vfloat handle;
-        const label lab;
+        h_vfloat handle; //the underlying data container
+        const label lab; //the name of the product that is used
+
     public:
-        SimpleGetter(const char* label, const char* instance, const char* process)
+        SimpleGetterVFloat(const char* label, const char* instance, const char* process)
         : handle()
         , lab(make_tuple(label, instance, process))
         {
@@ -33,23 +41,81 @@ class SimpleGetter {
 
 //It is convenient to group objects to namespaces
 //Instead of copying this for electrons etc, one should generalize!
-class mu
+class Lepton 
 {
 
-private:
-static const char *src;
-static SimpleGetter _pt;
-static SimpleGetter _eta;
-static SimpleGetter _phi;
+protected:
+//Internal interface
+const char *src;
 
-public:
-const static float pt(fw_event *ev)
-{
-    return value(_pt.get(ev));
+//Handle, label pairs for the data in the edm ntuple
+SimpleGetterVFloat _pt;
+SimpleGetterVFloat _eta;
+SimpleGetterVFloat _phi;
+
+Lepton(const char* _src) :
+    src(_src),
+    _pt(SimpleGetterVFloat(_src, "Pt", PROCESS)),
+    _eta(SimpleGetterVFloat(_src, "Eta", PROCESS)),
+    _phi(SimpleGetterVFloat(_src, "Phi", PROCESS)) {
 }
+
+float get(fw_event *ev, SimpleGetterVFloat& g) {
+    return value(g.get(ev));
+}
+
 };
 
-const char* mu::src = "goodSignalMuonsNTupleProducer";
-SimpleGetter mu::_pt(mu::src, "Pt", PROCESS);
-SimpleGetter mu::_eta(mu::src, "Eta", PROCESS);
-SimpleGetter mu::_phi(mu::src, "Phi", PROCESS);
+
+
+} //namespace unstable
+
+// ----------------------
+// -- PUBLIC INTERFACE --
+// ----------------------
+// Anything that is exported here as public will probably not change in declaration(shape).
+// For example, you can always call stpol::stable::signal::muon.pt(events).
+// Precise definition(implementation) may change, so you need not know how the pt is accessed for the signal muon.
+// To make the existing analysis code work with a different "backend", such as a flat ROOT TTree instead of the EDM format,
+// one would have to implement this interface with exactly the same structure as here.
+namespace stable {
+   
+    //A type for which there is no physical value available
+    typedef double na_type;
+
+    //A placeholder value.
+    static const na_type NA = unstable::NA;
+
+    //Check if something was NA. Depends on the type of the NA, but in general will be kept stable.
+    bool is_na(na_type v) { return std::isnan(v); }
+
+    //Interfaces for the leptons
+    class Muon : public unstable::Lepton {
+        public:
+            Muon() : unstable::Lepton("goodSignalMuonsNTupleProducer") {
+            }
+
+            float pt(fw_event *ev) { return get(ev, _pt); };
+            float eta(fw_event *ev) { return get(ev, _eta); };
+            float phi(fw_event *ev) { return get(ev, _phi); };
+
+    };
+    
+    class Electron : public unstable::Lepton {
+        public:
+            Electron() : unstable::Lepton("goodSignalElectronsNTupleProducer") {
+            }
+            
+            float pt(fw_event *ev) { return get(ev, _pt); };
+            float eta(fw_event *ev) { return get(ev, _eta); };
+            float phi(fw_event *ev) { return get(ev, _phi); };
+    };
+   
+    namespace signal {
+        static Muon muon;
+        static Electron electron;
+    }
+
+} //namespace stable
+
+} //namespace stpol
