f2fa70e50e88c87b1ebe7297bf5fa4b370e1b810
Joosep Pata <joosep.pata@gmail.com>
2013-10-01 14:03:55 +0300

    changed interface

--- CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/example/eventloop_example.cc
+++ CMSSW_5_3_11/src/SingleTopPolarization/Analysis/bin/example/eventloop_example.cc
@@ -1,5 +1,4 @@
 //This file can be copied as an example for your own loop
-#include "ROOT.jl/src/fwlite.hh"
 #include "src/headers/stpol.hh" //present in $STPOL_DIR/src/headers/stpol.hh
 
 int main(int argc, char **argv)
@@ -18,7 +17,7 @@ int main(int argc, char **argv)
     {
         events->to(i);
 
-        float pt = mu::get_pt(events); //defined in stpol.hh
+        float pt = mu::pt(events); //defined in stpol.hh
         std::cout << pt << std::endl;
     }
 
