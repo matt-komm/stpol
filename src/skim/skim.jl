9cb39ab6da0296491a00c19350ede98276b1763c
Joosep Pata <joosep.pata@gmail.com>
2013-10-03 15:21:33 +0300

    added skimming script

--- src/skim/skim.jl
+++ src/skim/skim.jl
@@ -13,20 +13,19 @@ end
 events = Events(convert(Vector{ASCIIString}, flist))
 
 maxev = length(events)
+#maxev = 100000
 
 processed_files = DataFrame(files=flist)
 df = similar(
         DataFrame(
             lepton_pt=Float32[], lepton_type=ASCIIString[], lepton_id=Int32[],
-            bjet_pt=Float32[], bjet_eta=Float32[], bjet_id=Float32[],
-            ljet_pt=Float32[], ljet_eta=Float32[], ljet_id=Float32[],
+            bjet_pt=Float32[], bjet_eta=Float32[], bjet_id=Float32[], bjet_bd_a=Float32[],
+            ljet_pt=Float32[], ljet_eta=Float32[], ljet_id=Float32[], ljet_bd_a=Float32[],
             run=Int64[], lumi=Int64[], event=Int64[], fileindex=Int64[],
             passes=Bool[]
         ),
         maxev
 )
-
-
 
 const sources = Dict{Symbol, Source}()
 
@@ -34,25 +33,18 @@ function part(x, y)
     return symbol(string(x, "_", y))
 end
 
+#leptons
 for s in [:Pt, :Eta, :Phi, :relIso, :genPdgId]
     sources[part(:muon, s)] = Source(:goodSignalMuonsNTupleProducer, s, :STPOLSEL2)
     sources[part(:electron, s)] = Source(:goodSignalElectronsNTupleProducer, s, :STPOLSEL2)
 end
 
-for s in [:Pt, :Eta, :Phi, :partonFlavour]
+#jets
+for s in [:Pt, :Eta, :Phi, :partonFlavour, :bDiscriminatorCSVMVA, :bDiscriminatorTCHP]
     sources[part(:bjet, s)] = Source(:highestBTagJetNTupleProducer, s, :STPOLSEL2)
     sources[part(:ljet, s)] = Source(:lowestBTagJetNTupleProducer, s, :STPOLSEL2)
 end
 
-function either(a, b)
-    if length(a)==1 && length(b)==0
-        return (a[1], :first)
-    elseif length(b)==1 && length(a)==0
-        return (b[1], :second)
-    else
-        return (NA, :neither)
-    end
-end
 
 function ifpresent(arr, n::Integer=1)
     if length(arr)==n
@@ -62,9 +54,27 @@ function ifpresent(arr, n::Integer=1)
     end
 end
 
+function either(a, b, n::Integer=1)
+    if length(a)==n && length(b)==0
+        return (a[n], :first)
+    elseif length(b)==n && length(a)==0
+        return (b[n], :second)
+    else
+        return (NA, :neither)
+    end
+end
+
+
 nproc = 0
+tic()
 timeelapsed = @elapsed for i=1:maxev
     nproc += 1
+
+    if nproc%(floor(maxev/20)) == 0
+        println("$nproc ($(floor(nproc/maxev*100.0))%) events processed")
+        toc()
+        tic()
+    end
     to!(events, i)
 
     df[i, :passes] = false
@@ -85,6 +95,7 @@ timeelapsed = @elapsed for i=1:maxev
     df[i, :lepton_type] = string(lepton_type)
 
     if which_lepton == :neither
+        df[i, :passes] = false
         continue
     end
 
@@ -95,24 +106,29 @@ timeelapsed = @elapsed for i=1:maxev
     df[i, :bjet_eta] = events[sources[:bjet_Eta]] |> ifpresent
     #df[i, :bjet_phi] = events[sources[:bjet_Phi]]
     df[i, :bjet_id] = events[sources[:bjet_partonFlavour]] |> ifpresent
+    df[i, :bjet_bd_a] = events[sources[:bjet_bDiscriminatorTCHP]] |> ifpresent
 
     df[i, :ljet_pt] = events[sources[:ljet_Pt]] |> ifpresent
     df[i, :ljet_eta] = events[sources[:ljet_Eta]] |> ifpresent
     #df[i, :ljet_phi] = events[sources[:ljet_Phi]]
     df[i, :ljet_id] = events[sources[:ljet_partonFlavour]] |> ifpresent
+    df[i, :ljet_bd_a] = events[sources[:ljet_bDiscriminatorTCHP]] |> ifpresent
 
     df[i, :passes] = true
 
 end
 
 println("processed $(nproc/timeelapsed) events/second")
-df = df[with(df, :(passes)), :]
-writetable("output.csv", df)
-writetable("processed.csv", processed_files)
 
-# using HDF5
-# using JLD
-# file = jldopen("output.jld", "w")
-# write(file, "df", df)
-# write(file, "processed_files", processed_files)
-# close(file)
+#Select only the events that have a lepton
+#df = df[with(df, :(passes)), :]
+
+
+function writezipped(fn, t)
+    writetable(fn, t)
+
+    run(`gzip -f9 $fn`)
+end
+
+writezipped("output.csv", df)
+writezipped("processed.csv", processed_files)
