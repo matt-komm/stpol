fab6a4337fef202594bab196ea348da93eab2e9a
Joosep Pata <joosep.pata@gmail.com>
2013-10-03 22:37:27 +0300

    use cmdline args

--- src/skim/skim.jl
+++ src/skim/skim.jl
@@ -1,14 +1,10 @@
 using ROOT
 using DataFrames
 
-files = {
-    :tchan => "tchan.txt"
-}
+output_file = ARGS[1]
 
 flist = Any[]
-for (k, fname) in files
-    append!(flist, split(open(readall, fname)))
-end
+append!(flist, ARGS[2:])
 
 events = Events(convert(Vector{ASCIIString}, flist))
 
@@ -19,10 +15,11 @@ processed_files = DataFrame(files=flist)
 df = similar(
         DataFrame(
             lepton_pt=Float32[], lepton_type=ASCIIString[], lepton_id=Int32[],
-            bjet_pt=Float32[], bjet_eta=Float32[], bjet_id=Float32[], bjet_bd_a=Float32[],
-            ljet_pt=Float32[], ljet_eta=Float32[], ljet_id=Float32[], ljet_bd_a=Float32[],
+            bjet_pt=Float32[], bjet_eta=Float32[], bjet_id=Float32[], bjet_bd_a=Float32[], bjet_bd_b=Float32[],
+            ljet_pt=Float32[], ljet_eta=Float32[], ljet_id=Float32[], ljet_bd_a=Float32[], ljet_bd_b=Float32[],
             run=Int64[], lumi=Int64[], event=Int64[], fileindex=Int64[],
-            passes=Bool[]
+            passes=Bool[],
+            fname=ASCIIString[]
         ),
         maxev
 )
@@ -82,6 +79,8 @@ timeelapsed = @elapsed for i=1:maxev
     df[i, :run], df[i, :lumi], df[i, :event] = where(events)
     df[i, :fileindex] = where_file(events)
     
+    df[i, :fname] = flist[df[i, :fileindex]]
+    
 
     df[i, :lepton_pt], which_lepton = either(events[sources[:muon_Pt]], events[sources[:electron_Pt]])
 
@@ -107,21 +106,22 @@ timeelapsed = @elapsed for i=1:maxev
     #df[i, :bjet_phi] = events[sources[:bjet_Phi]]
     df[i, :bjet_id] = events[sources[:bjet_partonFlavour]] |> ifpresent
     df[i, :bjet_bd_a] = events[sources[:bjet_bDiscriminatorTCHP]] |> ifpresent
+    df[i, :bjet_bd_b] = events[sources[:bjet_bDiscriminatorCSVMVA]] |> ifpresent
 
     df[i, :ljet_pt] = events[sources[:ljet_Pt]] |> ifpresent
     df[i, :ljet_eta] = events[sources[:ljet_Eta]] |> ifpresent
     #df[i, :ljet_phi] = events[sources[:ljet_Phi]]
     df[i, :ljet_id] = events[sources[:ljet_partonFlavour]] |> ifpresent
     df[i, :ljet_bd_a] = events[sources[:ljet_bDiscriminatorTCHP]] |> ifpresent
+    df[i, :ljet_bd_b] = events[sources[:ljet_bDiscriminatorCSVMVA]] |> ifpresent
 
     df[i, :passes] = true
-
 end
 
 println("processed $(nproc/timeelapsed) events/second")
 
 #Select only the events that have a lepton
-#df = df[with(df, :(passes)), :]
+df = df[with(df, :(passes)), :]
 
 
 function writezipped(fn, t)
@@ -130,5 +130,5 @@ function writezipped(fn, t)
     run(`gzip -f9 $fn`)
 end
 
-writezipped("output.csv", df)
-writezipped("processed.csv", processed_files)
+writezipped("$(output_file)", df)
+#writezipped("processed.csv", processed_files)
