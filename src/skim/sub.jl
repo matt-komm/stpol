294836129fe73fa826326f749e0a64ecdaa6756a
Joosep Pata <joosep@phys.hep.kbfi.ee>
2013-10-11 15:08:57 +0300

    output to folder

--- src/skim/sub.jl
+++ src/skim/sub.jl
@@ -1,7 +1,12 @@
 
 flist = Any[]
 
-for a in ARGS[1:]
+#output directory
+ofdir = ARGS[1]
+isdir(ofdir) || mkdir(ofdir)
+
+#input files
+for a in ARGS[2:]
     append!(flist, open(readall, a) |> split)
 end
 
@@ -11,21 +16,21 @@ function submit(infiles, outfile, i::Integer)
     end
     infilelist = join(infiles, " ")
     cmd="#!/bin/bash
-~/.julia/ROOT.jl/julia skim.jl $outfile $infilelist
+~/.julia/ROOT.jl/julia skim.jl $ofdir/$outfile $infilelist
 echo 'done '\$?
 "
-    fn = tempname()
+    fn = "$ofdir/job.$i"
     fi = open(fn, "w")
     write(fi, cmd)
     close(fi)
     println("Temp file is $fn")
-    ofile = "slurm.out.$i" 
+    ofile = "$ofdir/slurm.out.$i" 
     run(`sbatch -p phys,prio -J julia_job_test.$i -o $ofile $fn`)
 end
 
 maxn = length(flist)
 perjob = min(50, ceil(maxn/10))
-N = ceil(maxn/perjob)
+N = ceil(maxn/perjob)-1
 for n=1:N
     r = (1+(n-1)*perjob):(n*perjob)
     if r.start+r.len > maxn
