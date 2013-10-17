c6268d2ce33ee99d5cd1bf3cd3c8ba841857cd08
Joosep Pata <joosep.pata@gmail.com>
2013-10-17 10:25:16 +0300

    pull data files

--- sync_results.sh
+++ sync_results.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-DIR="~/singletop/stpol"
-rsync --progress --ignore-times -r cms.hep.kbfi.ee:$DIR/results ./
-rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/fit_histograms/no_powheg_fix results/
-rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/qcd_histograms results/
+DIR="~/singletop/stpol2"
+CMD="rsync --progress --ignore-times -r cms.hep.kbfi.ee"
 
-rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/fit_histograms/final results/fit_histograms/
-rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/unfolding_histograms/final results/unfolding_histograms/
+$CMD:$DIR/src/skim/data results/
+$CMD:$DIR/src/skim/tchan results/
+$CMD:$DIR/src/skim/ttjets results/
+$CMD:$DIR/src/skim/wjets results/
+
+#rsync --progress --ignore-times -r cms.hep.kbfi.ee:$DIR/results ./
+#rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/fit_histograms/no_powheg_fix results/
+#rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/qcd_histograms results/
+#
+#rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/fit_histograms/final results/fit_histograms/
+#rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/unfolding_histograms/final results/unfolding_histograms/
