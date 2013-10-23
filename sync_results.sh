f80720b09b784089fe5b24bfa3525b8ba87a3e10
Joosep Pata <joosep.pata@gmail.com>
2013-10-23 12:45:51 +0300

    sync all oct

--- sync_results.sh
+++ sync_results.sh
@@ -2,7 +2,7 @@
 DIR="~/singletop/stpol2"
 CMD="rsync --progress --ignore-times -r cms.hep.kbfi.ee"
 
-$CMD:$DIR/src/skim/oct17 results/
+$CMD:$DIR/src/skim/oct* results/
 
 #rsync --progress --ignore-times -r cms.hep.kbfi.ee:$DIR/results ./
 #rsync --progress --ignore-times -r cms.hep.kbfi.ee:/hdfs/local/stpol/fit_histograms/no_powheg_fix results/
