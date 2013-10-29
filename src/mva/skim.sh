5fbaee6f0fc18d4546ddce79d3789faf90d1b281
Joosep Pata <joosep.pata@cern.ch>
2013-10-29 09:20:36 +0200

    added tchpt

--- src/mva/skim.sh
+++ src/mva/skim.sh
@@ -2,6 +2,7 @@
 
 JULIA=~/local-sl6/julia/julia
 
+#CSVM
 for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
 do
     $JULIA ../skim/sub.jl skims/csvm/wjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
@@ -18,6 +19,7 @@ do
 done
 
 
+#CSVT
 for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
 do
     $JULIA ../skim/sub.jl skims/csvt/wjets/$fi  ../../filelists/Oct18_csvt_af8ee7/step2/mc/iso/nominal/Jul15/$fi.txt
@@ -31,4 +33,21 @@ done
 for fi in T_t_ToLeptons Tbar_t_ToLeptons
 do
     $JULIA ../skim/sub.jl skims/csvt/tchan/$fi  ../../filelists/Oct18_csvt_af8ee7/step2/mc/iso/nominal/Jul15/$fi.txt
+done
+
+#TCHPT
+DIR=../../filelists/343e0a9_Aug22/step2/mc/iso/nominal/Jul15
+for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
+do
+    $JULIA ../skim/sub.jl skims/tchpt/wjets/$fi  $DIR/$fi.txt
+done
+
+for fi in TTJets_FullLept TTJets_SemiLept
+do
+    $JULIA ../skim/sub.jl skims/tchpt/ttjets/$fi  $DIR/$fi.txt
+done
+
+for fi in T_t_ToLeptons Tbar_t_ToLeptons
+do
+    $JULIA ../skim/sub.jl skims/tchpt/tchan/$fi  $DIR/$fi.txt
 done
