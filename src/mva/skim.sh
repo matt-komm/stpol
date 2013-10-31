d68bd15f5925f8236a8cfdf0476ff9f898769422
Joosep Pata <joosep.pata@cern.ch>
2013-10-31 19:17:48 +0200

    removed csvm

--- src/mva/skim.sh
+++ src/mva/skim.sh
@@ -2,21 +2,21 @@
 
 JULIA=~/local-sl6/julia/julia
 
-#CSVM
-for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
-do
-    $JULIA ../skim/sub.jl skims/csvm/wjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
-done
-
-for fi in TTJets_FullLept TTJets_SemiLept
-do
-    $JULIA ../skim/sub.jl skims/csvm/ttjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
-done
-
-for fi in T_t_ToLeptons Tbar_t_ToLeptons
-do
-    $JULIA ../skim/sub.jl skims/csvm/tchan/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
-done
+##CSVM
+#for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
+#do
+#    $JULIA ../skim/sub.jl skims/csvm/wjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
+#done
+#
+#for fi in TTJets_FullLept TTJets_SemiLept
+#do
+#    $JULIA ../skim/sub.jl skims/csvm/ttjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
+#done
+#
+#for fi in T_t_ToLeptons Tbar_t_ToLeptons
+#do
+#    $JULIA ../skim/sub.jl skims/csvm/tchan/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
+#done
 
 
 #CSVT
