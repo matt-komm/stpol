6d4f0dad6e7cc98753988419f9a6730203c83444
Joosep Pata <joosep.pata@cern.ch>
2013-11-01 11:13:44 +0200

    removed TCHPT

--- src/mva/skim.sh
+++ src/mva/skim.sh
@@ -35,19 +35,19 @@ do
     $JULIA ../skim/sub.jl skims/csvt/tchan/$fi  ../../filelists/Oct18_csvt_af8ee7/step2/mc/iso/nominal/Jul15/$fi.txt
 done
 
-#TCHPT
-DIR=../../filelists/343e0a9_Aug22/step2/mc/iso/nominal/Jul15
-for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
-do
-    $JULIA ../skim/sub.jl skims/tchpt/wjets/$fi  $DIR/$fi.txt
-done
-
-for fi in TTJets_FullLept TTJets_SemiLept
-do
-    $JULIA ../skim/sub.jl skims/tchpt/ttjets/$fi  $DIR/$fi.txt
-done
-
-for fi in T_t_ToLeptons Tbar_t_ToLeptons
-do
-    $JULIA ../skim/sub.jl skims/tchpt/tchan/$fi  $DIR/$fi.txt
-done
+##TCHPT
+#DIR=../../filelists/343e0a9_Aug22/step2/mc/iso/nominal/Jul15
+#for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
+#do
+#    $JULIA ../skim/sub.jl skims/tchpt/wjets/$fi  $DIR/$fi.txt
+#done
+#
+#for fi in TTJets_FullLept TTJets_SemiLept
+#do
+#    $JULIA ../skim/sub.jl skims/tchpt/ttjets/$fi  $DIR/$fi.txt
+#done
+#
+#for fi in T_t_ToLeptons Tbar_t_ToLeptons
+#do
+#    $JULIA ../skim/sub.jl skims/tchpt/tchan/$fi  $DIR/$fi.txt
+#done
