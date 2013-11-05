0d23a1f0a4b7f7b2aff6644e2250d291f871ac8d
Joosep Pata <joosep.pata@cern.ch>
2013-11-05 11:15:16 +0200

    cleanup

--- src/mva/skim.sh
+++ src/mva/skim.sh
@@ -1,53 +1,23 @@
 #!/bin/bash
 
 JULIA=~/local-sl6/julia/julia
-
-##CSVM
-#for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
-#do
-#    $JULIA ../skim/sub.jl skims/csvm/wjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
-#done
-#
-#for fi in TTJets_FullLept TTJets_SemiLept
-#do
-#    $JULIA ../skim/sub.jl skims/csvm/ttjets/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
-#done
-#
-#for fi in T_t_ToLeptons Tbar_t_ToLeptons
-#do
-#    $JULIA ../skim/sub.jl skims/csvm/tchan/$fi  ../../filelists/Oct3_nomvacsv_nopuclean_e224b5/step2/mc/iso/nominal/Jul15/$fi.txt
-#done
-
+OFDIR=skims2
+FLDIR=../../filelists/Oct18_csvt_af8ee7
+MCDIR=step2/mc/iso/nominal/Jul15
 
 #CSVT
-for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
+DATANAME=csvt
+for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive WJets_inclusive
 do
-    $JULIA ../skim/sub.jl skims/csvt/wjets/$fi  ../../filelists/Oct18_csvt_af8ee7/step2/mc/iso/nominal/Jul15/$fi.txt
+    $JULIA ../skim/sub.jl $OFDIR/$DATANAME/wjets/$fi  $FLDIR/$MCDIR/$fi.txt
 done
 
-for fi in TTJets_FullLept TTJets_SemiLept
+for fi in TTJets_FullLept TTJets_SemiLept TTJets_MassiveBinDECAY
 do
-    $JULIA ../skim/sub.jl skims/csvt/ttjets/$fi  ../../filelists/Oct18_csvt_af8ee7/step2/mc/iso/nominal/Jul15/$fi.txt
+    $JULIA ../skim/sub.jl $OFDIR/$DATANAME/ttjets/$fi  $FLDIR/$MCDIR/$fi.txt
 done
 
-for fi in T_t_ToLeptons Tbar_t_ToLeptons
+for fi in T_t_ToLeptons Tbar_t_ToLeptons T_t Tbar_t
 do
-    $JULIA ../skim/sub.jl skims/csvt/tchan/$fi  ../../filelists/Oct18_csvt_af8ee7/step2/mc/iso/nominal/Jul15/$fi.txt
+    $JULIA ../skim/sub.jl $OFDIR/$DATANAME/tchan/$fi  $FLDIR/$MCDIR/$fi.txt
 done
-
-##TCHPT
-#DIR=../../filelists/343e0a9_Aug22/step2/mc/iso/nominal/Jul15
-#for fi in W1Jets_exclusive W2Jets_exclusive W3Jets_exclusive W4Jets_exclusive
-#do
-#    $JULIA ../skim/sub.jl skims/tchpt/wjets/$fi  $DIR/$fi.txt
-#done
-#
-#for fi in TTJets_FullLept TTJets_SemiLept
-#do
-#    $JULIA ../skim/sub.jl skims/tchpt/ttjets/$fi  $DIR/$fi.txt
-#done
-#
-#for fi in T_t_ToLeptons Tbar_t_ToLeptons
-#do
-#    $JULIA ../skim/sub.jl skims/tchpt/tchan/$fi  $DIR/$fi.txt
-#done
