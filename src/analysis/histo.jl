4bb83b7fc8494e3e3b28b1d355407d12c8934327
Joosep Pata <joosep.pata@gmail.com>
2013-11-25 13:57:05 +0200

    added some comments

--- src/analysis/histo.jl
+++ src/analysis/histo.jl
@@ -1,15 +1,25 @@
 module Hist
 
 using DataFrames
-using PyCall
+using DataArrays
 
+using PyCall
 @pyimport numpy
 
-import Base.+, Base.*, Base./
+using PyPlot
+
+import Base.+, Base.-, Base.*, Base./
 immutable Histogram
-    bin_entries::Vector{Int64}
-    bin_contents::Vector{Float64}
-    bin_edges::Vector{Float64}
+    bin_entries::Vector{Int64} #n values
+    bin_contents::Vector{Float64} #n values
+    bin_edges::Vector{Float64} #n+1 values, lower edges of all bins + upper edge of last bin
+
+    function Histogram(entries, contents, edges)
+        @assert length(entries)==length(contents) "entries and contents vector must be of equal length"
+        @assert length(entries)==length(edges)-1 "must specify n+1 edges"
+        @assert all(entries .>= 0.0) "number of entries must be >= 0"
+        new(entries, contents, edges)
+    end
 end
 
 function Histogram(n::Integer, low::Number, high::Number)
@@ -24,6 +34,10 @@ function Histogram(n::Integer, low::Number, high::Number)
 end
 
 Histogram(h::Histogram) = Histogram(h.bin_entries, h.bin_contents, h.bin_edges)
+
+function errors(h::Histogram)
+    return h.bin_contents .* sqrt(h.bin_entries) ./ h.bin_entries
+end
 
 function hfill!(h::Histogram, v::Real, w::Real=1.0)
     idx = searchsorted(h.bin_edges, v)
@@ -45,12 +59,25 @@ function +(h1::Histogram, h2::Histogram)
     return h
 end
 
-function *(h1::Histogram, x::Real)
+-(h1::Histogram, h2::Histogram) = h1 + (-1.0 * h2)
+
+function *{T <: Real}(h1::Histogram, x::T)
     return Histogram(h1.bin_entries, h1.bin_contents * x, h1.bin_edges)
 end
 
-function /(h1::Histogram, x::Real)
+function *{T <: Real}(x::T, h1::Histogram)
+    return h1 * x
+end
+
+
+function /{T <: Real}(h1::Histogram, x::T)
     return h1 * (1.0/x)
+end
+
+function /(h1::Histogram, h2::Histogram)
+    #warn("ratio plot errors are currently incorrect")
+    @assert(h1.bin_edges == h2.bin_edges, "bin edges must be the same for both histograms")
+    return Histogram(h1.bin_entries, h1.bin_contents ./ h2.bin_contents, h1.bin_edges)
 end
 
 function integral(h::Histogram)
@@ -58,12 +85,19 @@ function integral(h::Histogram)
 end
 
 function integral(h::Histogram, x1::Real, x2::Real)
+    if !(x1 in h.bin_edges) || !(x2 in h.bin_edges)
+        warn("integration will be inexact due to binning")
+    end
     a = searchsorted(h.bin_edges, x1).start
     b = searchsorted(h.bin_edges, x2).start
     return sum(h.bin_contents[a:b])
 end
 
-function norm!(h::Histogram)
+#returns the low edges of a list of histogram edges
+lowedge(arr) = arr[1:length(arr)-1];
+widths(arr) = [arr[i+1]-arr[i] for i=1:length(arr)-1]
+
+function normed(h::Histogram)
     i = integral(h)
     return i > 0 ? h/i : error("histogram integral was $i")
 end
@@ -75,8 +109,62 @@ fromdf(df::DataFrame) = Histogram(df[:, :bin_entries].data, df[:, :bin_contents]
 histogramdd(args...;kwargs...) = numpy.histogramdd(args..., kwargs...);
 flatten(h) = reshape(h, prod(size(h)))
 
-export Histogram, hfill!, hplot, integral, norm!
-export +, *, /
+function hplot(figure::PyObject, h::Histogram, prevhist::Histogram;kwargs...)
+    figure[:bar](lowedge(h.bin_edges), h.bin_contents, widths(h.bin_edges), prevhist.bin_contents; kwargs...)
+end
+
+function eplot(figure::PyObject, h::Histogram;kwargs...)
+    #figure[:plot](midpoints(h.bin_edges), h.bin_contents; kwargs...)
+    figure[:errorbar](midpoints(h.bin_edges), h.bin_contents, errors(h); kwargs...)
+end
+
+
+function eplot(figure::PyObject, hs::Vector{Histogram};kwargs...)
+   for h in hs
+        eplot(figure, h; kwargs...)
+   end
+end
+
+function hplot{T <: Number}(figure::PyObject, h::Histogram, prevval::T;kwargs...)
+    prevh = Histogram(
+        Int64[1 for i=1:length(h.bin_entries)],
+        Float64[convert(Float64, prevval) for i=1:length(h.bin_entries)],
+        h.bin_edges
+    )
+    hplot(figure, h, prevh; kwargs...)
+end
+
+function hplot(figure::PyObject, h::Histogram; kwargs...)
+    return hplot(figure, h, 0.0*h; kwargs...)
+end
+
+function hplot(figure::PyObject, hists::Vector{Histogram}, args=Dict[]; common_args=Dict())
+    ret = Any[]
+    for i=1:length(hists)
+
+        h = hists[i]
+        
+        arg = i <= length(args) ? args[i] : Dict()
+        argd = {k=>v for (k, v) in merge(arg, common_args)}
+
+        if !haskey(argd, :color)
+            argd[:color] = figure[:_get_lines][:color_cycle][:next]()
+        end
+
+        prevh = i>1 ? sum(hists[1:i-1]) : 0.0
+
+        r = hplot(figure, h, prevh; argd...)
+        
+        push!(ret, r)
+    end
+    return ret
+end
+
+export Histogram, hfill!, hplot, integral, normed, errors
+export +, -, *, /
 export todf, fromdf
 export histogramdd, flatten
+
+export hplot, eplot
+
 end #module
