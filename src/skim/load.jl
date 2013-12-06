2d1161a833d412c9ad2a46e2cc535c6847d1c100
Joosep Pata <joosep.pata@gmail.com>
2013-12-06 13:06:36 +0200

    added timing

--- src/skim/load.jl
+++ src/skim/load.jl
@@ -2,12 +2,12 @@ using HDF5,JLD
 using DataArrays, DataFrames
 inf = ARGS[1]
 
+tic()
+al = @allocated let 
 fi = jldopen(inf, "r")
 nam = names(fi)
 println("names=", join(nam, ","))
 
-el = @elapsed df = read(fi, "df");
+df = read(fi, "df");
 close(fi)
 println(size(df))
-println("Opening time: $el seconds")
-describe(df)
