ad263086647cd058dd7634615c675b96958c8d14
Joosep Pata <joosep.pata@gmail.com>
2013-12-15 13:45:19 +0200

    updated paths, variable

--- src/analysis/base.jl
+++ src/analysis/base.jl
@@ -3,20 +3,20 @@ using HDF5, JLD, DataFrames, DataArrays;
 
 include("../analysis/selection.jl")
 
-#const BASE="/Users/joosep/Documents/stpol/"
-const BASE="/home/joosep/singletop/stpol2/src/skim";
+@osx_only const BASE="/Users/joosep/Documents/stpol/"
+@linux_only const BASE="/home/joosep/singletop/stpol2/";
 
 const DEBUG=("DEBUG" in keys(ENV) && int(ENV["DEBUG"])==1)
 if DEBUG
     println("*** DEBUG mode activated")
 end
 
-const pdir = "output/plots"
-const hdir = "output/hists"
-const ydir = "output/yields"
-
-for d in [pdir, hdir, ydir]
-    mkpath(d)
-end
+const PDIR = "output/plots"
+const HDIR = "output/hists"
+const YDIR = "output/yields"
+const FITDIR = "output/fits"
 
 readdf(fn) = read(jldopen(fn), "df")
+
+t = readtable("$BASE/src/analysis/varnames.csv";separator=',');
+vars = {symbol(t[i, 1]) => t[i, 2] for i=1:nrow(t)};
