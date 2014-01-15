150fc68ad59524e0e6042234568e55806319c940
Joosep Pata <joosep.pata@cern.ch>
2014-01-15 09:38:33 +0200

    cleanup

--- src/analysis/evloop.jl
+++ src/analysis/evloop.jl
@@ -1,6 +1,7 @@
 #!/home/joosep/.julia/ROOT/julia
 include("histo.jl");
 include("base.jl");
+include("../skim/xs.jl");
 using ROOT, DataFrames, Hist
 
 inf = ARGS[1]
@@ -18,7 +19,8 @@ for k in [
     "sample",
     "cos_theta_lj", "xsweight", "pu_weight",
     "njets", "ntags", "lepton_type", "met", "mtw",
-    "bdt_sig_bg", "C", "lepton_pt", "ljet_eta"
+    "bdt_sig_bg", "C", "lepton_pt", "ljet_eta",
+    "bjet_dr", "ljet_dr"
     ]
     ROOT.set_branch_status!(df.tree, "$k*", true);
 end
@@ -28,56 +30,40 @@ NB = 0
 N = 0
 
 res = Dict()
+nbins = 119
 hdescs = {
-  :cos_theta_lj=>(120, -1, 1),
-  :met=>(120, 0, 300),
-  :mtw=>(120, 0, 300),
-  :bdt_sig_bg=>(120, -1, 1),
-  :C=>(120, 0, 1),
-  :lepton_pt=>(120, 0, 300),
-  :ljet_eta=>(120, -5, 5)
+  :cos_theta_lj=>(nbins, -1, 1),
+  :met=>(nbins, 0, 300),
+  :mtw=>(nbins, 0, 300),
+  :bdt_sig_bg=>(nbins, -1, 1),
+  :C=>(nbins, 0, 1),
+  :lepton_pt=>(nbins, 0, 300),
+  :ljet_eta=>(nbins, -5, 5),
 }
 
 println("looping over ", nrow(df), " events")
 
-function createhist(res, k, var)
+function gethist(res, k, var)
   if !in(k, keys(res))
     res[k] = Histogram(hdescs[var]...)
   end
-end
-
-function fillhists(res, k, cache)
-  for var in [:cos_theta_lj, :met, :mtw]
-    if !in(var, keys(cache))
-      cache[var] = df.tree[var]
-    end
-
-    k1 = "$(k)/$(var)"
-    createhist(res, "$(k1)/unweighted", var)
-    hfill!(
-      res["$k1/unweighted"],
-      cache[var],
-      cache[:xsweight]
-    )
-
-    createhist(res, "$(k1)/pu_weighted", var)
-    hfill!(
-      res["$k1/pu_weighted"],
-      cache[var],
-      cache[:xsweight] * cache[:pu_weight]
-    )
-  end
+  return res[k]
 end
 
 for i=1:nrow(df)
   cache = Dict()
+  
   if (i%10000 == 0)
       print(".")
   end
+  
   NB += ROOT.getentry!(df.tree, i);
   
   cache[:xsweight] = df.tree[:xsweight]
   cache[:pu_weight] = df.tree[:pu_weight]
+
+  cache[:pu_weight__up] = 1.1 * df.tree[:pu_weight]
+  cache[:pu_weight__down] = 0.9 * df.tree[:pu_weight]
 
   n_signal_mu = df.tree[:n_signal_mu]
   n_signal_ele = df.tree[:n_signal_ele]
@@ -104,43 +90,72 @@ for i=1:nrow(df)
 
   njets = df.tree[:njets]
   ntags = df.tree[:ntags]
+  (isna(ntags) || ntags<=0) && continue
 
   syst = df.tree[:systematic]
-  sample = df.tree[:sample]
   
   lepton = int(df.tree[:lepton_type])
   isna(lepton) && continue
-
-  iso = df.tree[:isolation]
+  
+  if lepton==13
+      leptype=:muon
+  elseif lepton==11
+      leptype=:electron
+  else
+      continue
+  end
   
   passes_met = false
+  
   met = df.tree[:met]
   mtw = df.tree[:mtw]
   (isna(met) || isna(mtw)) && continue
+
+  !((leptype==:electron && met > 45) || (leptype==:muon && mtw > 50)) && continue
+  passes_met = true
+    
+  bjet_dr = df.tree[:bjet_dr]
+  ljet_dr = df.tree[:ljet_dr]
+
+  if (isna(bjet_dr) || isna(ljet_dr) || bjet_dr < 0.5 || ljet_dr < 0.5)
+    continue
+  end
+
+  cache[:cos_theta_lj] = df.tree[:cos_theta_lj]
+  sample = hmap[:from][int(df.tree[:sample])]
+  syst = hmap[:from][int(df.tree[:systematic])]
+  iso = hmap[:from][int(df.tree[:isolation])]
+
+  weight = 1.0
+  if ((sample != "data_mu") && (sample != "data_ele"))
+      weight = weight * cache[:xsweight] * cache[:pu_weight]
+  end
   
-  k = "$(iso)/$(lepton)/$(njets)j/$(ntags)t/met_off/$(syst)/$(sample)"
-  fillhists(res, k, cache)
+  weights = Dict()
+  if syst == "nominal"
+      weights[:pu_weight__up] = weight * cache[:pu_weight__up] / cache[:pu_weight]
+      weights[:pu_weight__down] = weight * cache[:pu_weight__down] / cache[:pu_weight]
+      #weights[:lepton_weight__up] = weight * df.tree[:lepton_weight__up] / cache[:lepton_weight]
+      #weights[:lepton_weight__down] = weight * df.tree[:lepton_weight__down] / cache[:lepton_weight]
+  end
+
+  bdt = df.tree[:bdt_sig_bg]
+  for bdt_cut in linspace(-1, 1, 21)
+    bdt < bdt_cut && continue
+    bdts = @sprintf("%.2f", bdt_cut)
+    
+    hn ="$leptype/$iso/$syst/$njets/$ntags/$sample/met__$passes_met/$bdts" 
+    
+    h = gethist(res, hn, :cos_theta_lj)
+    hfill!(h, cache[:cos_theta_lj], weight)
+    
+    for (wn, w) in weights 
+        h = gethist(res, "$hn/$wn", :cos_theta_lj)
+        hfill!(h, cache[:cos_theta_lj], w)
+    end
+
+  end
   
-  if (met>45)
-    k = "$(iso)/$(lepton)/$(njets)j/$(ntags)t/met_45/$(syst)/$(sample)"
-    fillhists(res, k, cache)
-  end
-  if (met>55)
-    k = "$(iso)/$(lepton)/$(njets)j/$(ntags)t/met_55/$(syst)/$(sample)"
-    fillhists(res, k, cache)
-  end
-
-  if (mtw>50)
-    k = "$(iso)/$(lepton)/$(njets)j/$(ntags)t/mtw_50/$(syst)/$(sample)"
-    fillhists(res, k, cache)
-  end
-  if (mtw>60)
-    k = "$(iso)/$(lepton)/$(njets)j/$(ntags)t/mtw_60/$(syst)/$(sample)"
-    fillhists(res, k, cache)
-  end
-
-  !((lepton==11 && met > 45) || (lepton==13 && mtw > 50)) && continue
-
   N += 1
 end
 
@@ -148,24 +163,7 @@ NB=NB/1024/1024
 q = toq();
 println("$N $(nrow(df)) $(NB)Mb $(q)s")
 
-#fnames = ASCIIString[]
-#dtypes = ASCIIString[]
-#
-#for k in keys(res)
-#  fname = "$ofname/$k.csv"
-#  mkpath(dirname(fname))
-#  if typeof(res[k]) <: Histogram
-#    writetable(fname, todf(res[k]))
-#    push!(fnames, fname)
-#    push!(dtypes, string(typeof(res[k])))
-#  end
-#end
-#metadata = DataFrame(fnames=fnames, dtypes=dtypes)
-#writetable("$ofname/metadata.csv", metadata)
-#
-#run(`tar -zcvf $ofname.tgz $ofname`)
-#run(`rm -Rf $ofname`)
-
 of = jldopen(ofname, "w")
 write(of, "res", res)
+println("wrote $(length(res)) histograms")
 close(of)
