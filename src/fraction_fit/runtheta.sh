0c6af68c3fd33d962f691726d203aa1279455399
Joosep Pata <joosep.pata@gmail.com>
2014-01-18 17:27:24 +0200

    run theta from relative path

--- src/fraction_fit/runtheta.sh
+++ src/fraction_fit/runtheta.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- DYLD_LIBRARY_PATH=/Users/joosep/Documents/stpol/theta/lib ./rootwrap.sh /Users/joosep/Documents/stpol/theta/utils2/theta-auto.py $@
+DYLD_LIBRARY_PATH=../../theta/lib ./rootwrap.sh ../../theta/utils2/theta-auto.py $@
