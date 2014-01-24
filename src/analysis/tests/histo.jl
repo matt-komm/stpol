b3a1f838208a24cc58be8012fe4493ea4ab3df38
Joosep Pata <joosep.pata@gmail.com>
2014-01-24 23:48:43 +0200

    more histogram tests with root

--- src/analysis/tests/histo.jl
+++ src/analysis/tests/histo.jl
@@ -1,5 +1,6 @@
 include("../histo.jl")
 using Hist
+include("../hroot.jl")
 using Base.Test
 
 @test_throws Histogram([1],[1],[0,1])
@@ -23,6 +24,12 @@ hfill!(h, -0.999)
 @test(h.bin_entries[2]==1)
 hfill!(h, NaN)
 @test(h.bin_entries[1]==2)
+hfill!(h, NA)
+@test(h.bin_entries[1]==3)
+
+tf = tfile("test.root", "RECREATE")
+toroot(h, tf, "some/other/dir/h1")
+close(tf)
 
 h = Histogram([1],[1],[0])
 @test integral(h)==1
@@ -55,8 +62,49 @@ h = Histogram([1,2],[3,4],[1,2])
 @test_approx_eq_eps(test_ks(h, 3.3*h), 0.0, 0.00001)
 
 
-nh = NHistogram({[-1, 1], [-1, 1]})
-@test nbins(nh)==4
-@test findbin_nd(nh, [0.1, 0.1]) == (1,1)
-@test findbin_nd(nh, [1.1, 0.1]) == (2,1)
-@test findbin_nd(nh, [0.1, 1.1]) == (1,2)
+nh = NHistogram({[-1, 0, 1], [-1, 0, 1]})
+@test nbins(nh)==9
+@test ndim(nh)==2
+@test findbin_nd(nh, [0.1, 0.1]) == (2,2)
+@test findbin_nd(nh, [1.1, 0.1]) == (3,2)
+@test findbin_nd(nh, [0.1, 1.1]) == (2,3)
+
+hfill!(nh, {NA, NA})
+@test nh[1,1] == 1
+
+hfill!(nh, {0.5, 0.5})
+hfill!(nh, {0.5, 0.5})
+@test nh[2,2] == 2
+
+hfill!(nh, {0.5, 1.1})
+hfill!(nh, {0.5, 1.1})
+hfill!(nh, {0.5, 1.1})
+@test nh[2,3] == 3
+
+hfill!(nh, {1.1, 1.1})
+hfill!(nh, {1.1, 1.1})
+hfill!(nh, {1.1, 1.1})
+hfill!(nh, {1.1, 1.1})
+@test nh[3, 3] == 4
+
+writecsv("test_2d.csv", nh)
+@test isfile("test_2d.csv.json")
+@test isfile("test_2d.csv.hist")
+@test readhist("test_2d.csv") == nh
+
+nh = NHistogram({
+	vcat(-Inf, linspace(-3, 3, 20), Inf),
+	vcat(-Inf, linspace(-2, 2, 10), Inf)
+})
+for i=1:100000
+	x = randn()
+	y = rand()>0.8 ? NA : 0.2*randn()
+	hfill!(nh, {x, y})
+end
+writecsv("test_2d_large.csv", nh)
+@test readhist("test_2d_large.csv") == nh
+
+tf = tfile("test.root", "UPDATE")
+toroot(nh, tf, "my/nested/dir/h2d")
+toroot(nh, tf, "my/nested2/dir/h2d")
+close(tf)
