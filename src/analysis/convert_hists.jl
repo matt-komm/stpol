3977609335bba18ef78457079f127006225af3a7
Joosep Pata <joosep.pata@gmail.com>
2014-02-07 18:20:54 +0100

    renamed x<->y

--- src/analysis/convert_hists.jl
+++ src/analysis/convert_hists.jl
@@ -19,29 +19,24 @@ allkeys = filter(x->typeof(x)<:Dict, keys(hists))
 
 toc()
 
-
-#load the fit results
-frd = {
-    :mu=>FitResult("$BASE/results/bdt_fit/results/jan16/fits/2j1t_3j2t/mu/results.json"),
-    :ele=>FitResult("$BASE/results/bdt_fit/results/jan16/fits/2j1t_3j2t/ele/results.json")
-};
-
 tm = prep_transfermatrix(hists)
 for (k, v) in tm
-    #println("converting 2D histogram $k to $outdir/tmatrix.root/$k")
     toroot("$outdir/tmatrix.root", string(k), v::NHistogram, {"GEN", "RECO"})
 
     projx = project_x(v)
-    toroot("$outdir/tmatrix.root", "$(k)__proj_x", projx::Histogram, "RECO")
+    toroot("$outdir/tmatrix.root", "$(k)__proj_y", projx::Histogram, "RECO")
 
     projy = project_y(v)
-    toroot("$outdir/tmatrix.root", "$(k)__proj_y", projy::Histogram, "GEN")
+    toroot("$outdir/tmatrix.root", "$(k)__proj_x", projy::Histogram, "GEN")
+    #println("$k $(integral(v)) $(integral(projx)) $(integral(projy))")
 end
 
 
 for lep in [:mu, :ele]
-    h = merge_histogram(:mu, hists, :cos_theta_lj, 2, 1)
+    h = merge_histogram(lep, hists, :cos_theta_lj, 2, 1)
     od = "$outdir/$lep"
+    reweight_hists_to_fitres(FITRESULTS[lep], h)
+
     for (k, v) in h
         hi = toroot("$od/cos_theta_lj.root", "2j1t_cos_theta_lj__$(k)", v::Histogram, "RECO")
     end
@@ -50,14 +45,14 @@ for lep in [:mu, :ele]
 
         for (nj, nt) in [(2,1), (3,2)]
             ss = "$(nj)j$(nt)t"
-            h = merge_histogram(:mu, hists, :bdt_sig_bg, nj, nt)
+            h = merge_histogram(lep, hists, :bdt_sig_bg, nj, nt)
             for (k, v) in h
-                hi = toroot("$od/bdt_sig_bg_$(ss)__merged.root", "$(ss)_bdt_sig_bg__$(k)", v::Histogram, "RECO")
+                hi = toroot("$od/bdt_sig_bg_$(ss).root", "$(ss)_bdt_sig_bg__$(k)", v::Histogram, "RECO")
             end
 
             merged = mergehists_4comp(h)
             for (k, v) in merged
-                hi = toroot("$od/bdt_sig_bg_$(ss)__merged.root", "$(ss)_bdt_sig_bg__$(k)", v::Histogram, "RECO")
+                hi = toroot("$od/merged/bdt_sig_bg_$(ss).root", "$(ss)_bdt_sig_bg__$(k)", v::Histogram, "RECO")
             end
             writetable("$od/bdt_sig_bg_$(ss)__merged.csv", todf(merged))
         end
