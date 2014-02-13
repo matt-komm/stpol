5b97387a0e5d448cf0ac2ff3f7a2fa90e50c17e7
Joosep Pata <joosep.pata@cern.ch>
2014-02-13 15:17:33 +0200

    input scripts more general

--- src/mva/inp.sh
+++ src/mva/inp.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 #generated file lists
 
-find skims/$1/tchan -name "*.root" > input/$2/sig.txt
-find skims/$1/ttjets -name "*.root" > input/$2/bg.txt
-find skims/$1/wjets -name "*.root" >> input/$2/bg.txt
+SKIM_DIR="/hdfs/local/joosep/stpol/mva/"
+
+TAG=$1
+BDT=$2
+
+mkdir -p input/$BDT
+
+find $SKIM_DIR/$TAG/tchan -name "*.root" > input/$BDT/sig.txt
+find $SKIM_DIR/$TAG/ttjets -name "*.root" > input/$BDT/bg.txt
+find $SKIM_DIR/$TAG/wjets -name "*.root" >> input/$BDT/bg.txt
+
+#separate inclusive files
+grep "/T_t/" input/$BDT/sig.txt > input/$BDT/train.txt
+grep "/Tbar_t/" input/$BDT/sig.txt >> input/$BDT/train.txt
+grep "/TTJets_MassiveBinDECAY/" input/$BDT/bg.txt >> input/$BDT/train.txt
+grep "/WJets_inclusive/" input/$BDT/bg.txt >> input/$BDT/train.txt
