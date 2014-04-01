86de77a3e18df1338d4ed8aabba8af3336d8674e
Joosep Pata <joosep.pata@cern.ch>
2014-04-01 09:49:06 +0300

    run mvas

--- src/skim/sub.jl
+++ src/skim/sub.jl
@@ -10,10 +10,8 @@ isdir(ofdir) || mkpath(ofdir)
 flist = Any[]
 
 #input files
-for a in ARGS[2:length(ARGS)]
-    #split the file by lines
-    append!(flist, open(readall, a) |> split)
-end
+append!(flist, open(readall, ARGS[2])|>split)
+
 
 #submit a slurm job on a list of files (infiles) with output to outfile.
 #the job is given an index i
@@ -29,15 +27,18 @@ function submit(infiles, outfile, i::Integer)
 
     #fn = "$ofdir/job.$i"
     #ofile = "$ofdir/slurm.out.$i"
-    fn = "/home/joosep/singletop/output/skims/output/job.$i"
-    ofile = "/home/joosep/singletop/output/skims/output/slurm.out.$i"
+    
+    fn = "/home/joosep/singletop/output/skims/output/$ofdir/job.$i"
+    ofile = "/home/joosep/singletop/output/skims/output/$ofdir/slurm.out.$i"
     skimoutput = "$ofdir/skim.out.$i"
     scpath = dirname(Base.source_path())
 
-    subcmd = `sbatch --exclude=$scpath/exclude.txt -J julia_skim -o $ofile $fn`
+    subcmd = `sbatch --exclude=$scpath/exclude.txt -J julia_skim -p prio -o $ofile $fn`
+    #subcmd = `sbatch --exclude=$scpath/exclude.txt -J julia_skim $fn`
     
     #the submit script (indents matter)
     cmd="#!/bin/bash
+set -e
 uname -a
 echo \$SLURM_JOB_ID
 \ls -1 /hdfs &> /dev/null
@@ -45,9 +46,20 @@ RET=\$?
 if [ \$RET -ne 0 ]; then
     echo '/hdfs was not available'
 else
-    ~/.julia/CMSSW/julia $scpath/skim.jl $ofdir/$outfile $infilelist > $skimoutput
+    tmp=`mktemp` 
+    ~/.julia/CMSSW/julia $scpath/skim.jl $ofdir/$outfile $infilelist > \$tmp
+    
+    source ~/local-sl6/root/bin/thisroot.sh && ( python ../mva/adder.py bdt_sig_bg ../mva/weights/stpol_bdt_sig_bg_lepton_flavour.weights.xml $ofdir/$outfile.root ) 
+    source ~/local-sl6/root/bin/thisroot.sh && ( python ../mva/qcd_mva_adder.py $ofdir/$outfile.root )
+    source ~/local-sl6/root/bin/thisroot.sh && ( python ../mva/qcd_mva_adder2.py $ofdir/$outfile.root )
+    source ~/local-sl6/root/bin/thisroot.sh && ( python ../mva/top_13_001_mva_adder.py $ofdir/$outfile.root )
+    rm -f $skimoutput
+    cp \$tmp $skimoutput
+    rm -f \$tmp
     RET=\$?
 fi
+echo 'syncing...'
+sync;sleep 1
 echo 'done '\$RET && exit \$RET
 
 ###SUBCMD=$subcmd
@@ -55,18 +67,24 @@ echo 'done '\$RET && exit \$RET
 "
     
     #write the slurm script
+    isfile(fn) && rm(fn) 
+    run(`sync`)
+    mkpath(dirname(fn))
     fi = open(fn, "w")
     write(fi, cmd)
     close(fi)
-    
+    run(`sync`)
+    sleep(0.5)
+    (isfile(fn) && length(readall(fn)) > 0) || error("could not write job file $fn") 
     #run(subcmd)
+
     while true
         try
             run(subcmd)
             break
-        catch e
-            #println(e)
-            sleep(1)
+        catch err
+            warn(err)
+            sleep(0.5)
         end
     end
 end
@@ -76,7 +94,7 @@ maxn = length(flist)
 #perjob = min(100, ceil(maxn/10)) |> int
 empty_flist = deepcopy(flist)
 
-perjob=50
+perjob = int(ARGS[3])
 #N = int(ceil(maxn/perjob)-1)
 
 j = 1
@@ -97,7 +115,6 @@ for n=1:perjob:maxn
         splice!(empty_flist, findfirst(empty_flist, x)) 
     end
     j += 1
-    sleep(0.05)
 end
 
 length(empty_flist) ==0 || error("did not submit all jobs: $empty_flist")
