6cafcc341d6bbaa91e24e4097705a5abdb0613e8
Joosep Pata <joosep.pata@cern.ch>
2014-04-01 09:43:53 +0300

    deprecated

--- src/analysis/prepare_scan_cards.jl
+++ src/analysis/prepare_scan_cards.jl
@@ -1,27 +1,44 @@
+error("DEPRECATED")
 using JSON
 
-infiles = readall(`find /hdfs/local/joosep/stpol/skims/feb27/split/feb19/ -name "*.root"` |> `sort`)|>split
-infile(i) = "/hdfs/local/joosep/stpol/skims/feb27/split/feb19/output_$i.root"
-
-binning=vcat(-Inf, linspace(-1, 1, 11), Inf)
-
-function card(infile, n)
+function card(infile, outpath, n, qcd_cut)
     return {
         "infile"=>infile,
-        "outfile"=>"/home/joosep/singletop/output/hists2/$n.root",
-        #"outfile"=>"/hdfs/local/joosep/stpol/hists/$n.root",
+        "outfile"=>"$outpath/$n.root",
         "tm_nominal_only" => false, 
+        "qcd_cut" => qcd_cut,
     }
 end
 
-i = 1
-card_path = "input/bdt_scan/cards/"
-mkpath(card_path)
+#indir = "/hdfs/local/joosep/stpol/skims/feb27/split/feb19/"
+indir = 
 
-for i=1:length(infiles)
-    of = open("$card_path/bdt_$i.json", "w")
-    c = card(infile(i), i)
-    write(of, json(c))
-    close(of)
+function prepare_cards(indir, outpath, card_path, qcd_cut="mva_nominal") 
+    mkpath(card_path)
+    infiles = readall(`find $indir -name "*.root"` |> `sort`)|>split
+    infile(i) = "$indir/output_$i.root"
+    
+    i = 1
+    
+    for i=1:length(infiles)
+        of = open("$card_path/bdt_$i.json", "w")
+        c = card(infile(i), outpath, i, qcd_cut)
+        write(of, json(c))
+        close(of)
+    end
 end
 
+prepare_cards(
+    "/hdfs/local/joosep/stpol/skims/mar19",
+    "/home/joosep/singletop/output/hists/mar19", "input/hists/mar19"
+)
+
+prepare_cards(
+    "/hdfs/local/joosep/stpol/skims/mar19",
+    "/home/joosep/singletop/output/hists/mar19__qcd_metmtw_nominal", "input/hists/mar19__qcd_metmtw_nominal", "metmtw_nominal"
+)
+
+prepare_cards(
+    "/hdfs/local/joosep/stpol/skims/mar22_tchpt",
+    "/home/joosep/singletop/output/hists/mar22_tchpt", "input/hists/mar22_tchpt"
+)
