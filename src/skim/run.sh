4d1ef3c11740b96842f96f7ed639b265b653b552
Joosep Pata <joosep.pata@cern.ch>
2014-04-01 09:49:28 +0300

    added run script for grid-control

---
+++ src/skim/run.sh
@@ -0,0 +1,25 @@
+set -e
+\ls -1 /hdfs &> /dev/null
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo '/hdfs was not available'
+fi
+
+outfile="output"
+scpath=/home/joosep/Dropbox/kbfi/top/stpol/src/skim
+mvapath=$scpath/../mva
+
+~/.julia/CMSSW/julia $scpath/skim.jl $outfile $FILE_NAMES
+
+source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/adder.py bdt_sig_bg $mvapath/weights/stpol_bdt_sig_bg_lepton_flavour.weights.xml $outfile.root ) 
+source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/qcd_mva_adder.py $outfile.root )
+source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/qcd_mva_adder2.py $outfile.root )
+source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/top_13_001_mva_adder.py $outfile.root )
+\ls -1 .
+
+p=/hdfs/local/joosep/stpol/skims/@GC_DATE@/@DATASETPATH@/@MY_JOBID@
+rm -Rf $p
+mkdir -p $p
+rsync -c output* $p
+
+\ls -1 $p
