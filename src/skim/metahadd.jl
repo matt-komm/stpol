6d381bbd2b98d69b04753667d4eac56172143a2c
Joosep Pata <joosep.pata@cern.ch>
2014-04-07 08:23:49 +0300

    sorted processing

--- src/skim/metahadd.jl
+++ src/skim/metahadd.jl
@@ -7,38 +7,53 @@ include("../analysis/base.jl")
 fname = ARGS[1]
 ofile = ARGS[2]
 infile = ARGS[3]
-infiles = readall(infile)|>split
+infiles = sort(map(x -> convert(ASCIIString, strip(x)), readall(infile)|>split))|>collect
+processed = ASCIIString[]
 
-flist = split(readall(fname))
+flist = sort(map(
+    x -> convert(ASCIIString, strip(x)),
+    split(readall(fname))
+))|>collect
+
 @assert length(flist)>0 "no files specified"
 
 println("Running over $(length(flist)) files")
 
 n = 0
 res = Dict()
+
+processed2 = ASCIIString[]
 for fi in flist
+    fi in processed2 && error("double processing of $fi")
+    push!(processed2, fi)
+
     acc = accompanying(fi)
-    
     md = nothing
 
+    println(fi," ", acc["processed"])
     try
         md = readtable(acc["processed"], allowcomments=true)
     catch err
         warn("could not read table with processed=$(acc["processed"])")
         continue 
     end
-    println(fi," ", acc["processed"])
 
     for i=1:nrow(md)
-        f = md[i, :files]
+        f = convert(ASCIIString, strip(md[i, :files]))
         
-        f in infiles || error("unrecognized file $f, not in $infile")
-        splice!(infiles, findfirst(infiles, f))
+        #idx = searchsortedfirst(infiles, f)
+        #if !in(f, infiles)
+        #    println("unrecognized file $f, not in $infile")
+        #    error("unrecognized file $f, not in $infile")
+        #    continue
+        #end
+        f in processed && warn("double processing of $f")
+        push!(processed, f)
 
         st = sample_type(f)
         #println("$f $st")
-        sample, iso, systematic = st[:sample], st[:iso], st[:systematic]
-        k = "$(sample)/$(iso)/$(systematic)"
+        tag, sample, iso, systematic = st[:tag], st[:sample], st[:iso], st[:systematic]
+        k = "$(tag)/$(sample)/$(iso)/$(systematic)"
         if !haskey(res, k)
             res[k] = 0
             res["$(k)/counters/generated"] = 0
@@ -49,7 +64,10 @@ for fi in flist
     end
 end
 
-length(infiles)==0 || warn("incomplete processing, some files in input file but not in root: \n$infiles")
+processed = sort(processed)|>collect
+
+((length(infiles) == length(processed)) && (infiles .== processed)) ||
+    warn("incomplete processing, some files in input file but not in root")
 
 of = open(ofile, "w")
 write(of, json(res))
