a4037dd76b78477c37a1efff2e1b766d3d8dde45
Joosep Pata <joosep.pata@cern.ch>
2014-04-16 21:34:18 +0300

    added new variable names, title method

--- src/analysis/varnames.jl
+++ src/analysis/varnames.jl
@@ -19,6 +19,18 @@ const VARS = {
 	:cos_theta_lj_gen=>"generated \$ \\cos{\\ \\theta^*} \$",
 	:cos_theta_bl_gen=>"generated \$ \\cos{\\ \\theta}_{\\eta-bl} \$",
 	:ljet_rms=>"\$ RMS_{j'} \$",
-	:ljet_dr=>"\$\\Delta R_{j'}\$",
+	:ljet_dr=>"\$\\Delta R_{j',l}\$",
+	:bjet_dr=>"\$\\Delta R_{b,l}\$",
+	:n_good_vertices=>"\$N_{vertices}\$",
 	:bdt_qcd=>"\$BDT_{QCD}\$\ output",
 }
+
+const titles = {
+	:jet => ((nj, nt) -> ("%(nj)J(%(nt)T)")),
+	:lepton => (lep -> {:mu=>"\$\\mu^\\pm\$", :ele=>"\$\\mu^\\pm\$"}[lep]),
+}
+
+title(nj::Integer, nt::Integer, lepton::Symbol) =
+	string(titles[:lepton][lepton], ", ", titles[:jet](nj, nt))
+
+export VARS, titles
