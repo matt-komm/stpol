00df2415c3b89895f3d512dc8afa3d3c963f2cf6
andresti <andres.tiko@cern.ch>
2014-06-30 14:33:19 +0300

    PDF uncertainties histograms

---
+++ src/pdf_uncertainties/utils.py
@@ -0,0 +1,17 @@
+pdfs = ["CT10", "CT10as", "NNPDF23", "NNPDF23nloas0116LHgrid", "NNPDF23nloas0117LHgrid", "NNPDF23nloas0118LHgrid", "NNPDF23nloas0120LHgrid", "NNPDF23nloas0121LHgrid", "NNPDF23nloas0122LHgrid"]
+
+sizes = {}
+sizes["MSTW2008nlo68cl"] = 40
+sizes["NNPDF21"] = 100
+sizes["cteq66"] = 44
+sizes["CT10"] = 52
+sizes["CT10as"] = 10
+sizes["MSTW2008CPdeutnlo68cl"] = 46
+sizes["NNPDF23"] = 100
+sizes["NNPDF23nloas0119LHgrid"] = 100
+sizes["NNPDF23nloas0116LHgrid"] = 100
+sizes["NNPDF23nloas0117LHgrid"] = 100
+sizes["NNPDF23nloas0118LHgrid"] = 100
+sizes["NNPDF23nloas0120LHgrid"] = 100
+sizes["NNPDF23nloas0121LHgrid"] = 100
+sizes["NNPDF23nloas0122LHgrid"] = 100    
