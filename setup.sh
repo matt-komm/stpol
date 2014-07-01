40c95cf6784c705e9019151d5b1298b2958d846c
Joosep Pata <joosep.pata@cern.ch>
2014-07-01 14:45:19 +0300

    updated CMSSW version to 5_3_18

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ then
 fi
 
 #Tags for https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes52X#V08_09_43
-[[ ! -z "$CMSVERSION" ]] || CMSVERSION=CMSSW_5_3_11
+[[ ! -z "$CMSVERSION" ]] || CMSVERSION=CMSSW_5_3_18
 #echo "Stashing current working directory, use 'git stash pop' later to retrieve"
 git stash #temporaryily store changes
 rm -Rf $CMSVERSION #remove the source tree for cmsrel to work
@@ -31,49 +31,69 @@ source setenv.sh
 cd $CMSSW_BASE/src
 
 #From official PAT recipe
-addpkg DataFormats/PatCandidates V06-05-06-12
-addpkg PhysicsTools/PatAlgos     V08-09-62
-addpkg PhysicsTools/PatUtils #V03-09-28 FIXME
-addpkg RecoBTag/ImpactParameter V01-04-09-01
-addpkg RecoBTag/SecondaryVertex V01-10-06
-addpkg RecoBTag/SoftLepton      V05-09-11
-addpkg RecoBTau/JetTagComputer  V02-03-02
-addpkg RecoBTag/Configuration   V00-07-05
-addpkg RecoParticleFlow/PFProducer V15-02-06
-addpkg RecoLuminosity/LumiDB V04-02-08 #For lumicalc
-addpkg PhysicsTools/CandUtils V09-01-05 #For event shape
+#https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMETRecipe53X#CMSSW_5_3_14_patch1_or_later
+git cms-addpkg PhysicsTools/PatAlgos # PAT Recipe
+git cms-addpkg CommonTools/Utils
+git cms-addpkg PhysicsTools/Configuration
+git cms-addpkg PhysicsTools/PatExamples
+git cms-addpkg PhysicsTools/PatUtils
+git cms-addpkg PhysicsTools/SelectorUtils
+git cms-addpkg PhysicsTools/UtilAlgos
+git cms-addpkg RecoBTag/SoftLepton
+git cms-addpkg RecoMET/METFilters
+git cms-merge-topic -u cms-analysis-tools:5_3_13-newJetFlavour
+git cms-merge-topic -u cms-analysis-tools:5_3_13-updatedPATConfigs
+git cms-merge-topic -u TaiSakuma:53X-met-140217-01
+
+#addpkg DataFormats/PatCandidates V06-05-06-12
+#addpkg PhysicsTools/PatAlgos     V08-09-62
+#addpkg PhysicsTools/PatUtils #V03-09-28 FIXME
+#addpkg RecoBTag/ImpactParameter V01-04-09-01
+#addpkg RecoBTag/SecondaryVertex V01-10-06
+#addpkg RecoBTag/SoftLepton      V05-09-11
+#addpkg RecoBTau/JetTagComputer  V02-03-02
+#addpkg RecoBTag/Configuration   V00-07-05
+#addpkg RecoParticleFlow/PFProducer V15-02-06
+#addpkg RecoLuminosity/LumiDB V04-02-08 #For lumicalc
+#addpkg PhysicsTools/CandUtils V09-01-05 #For event shape
 
 #For electron MVA https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentification#Recipe_for_5_3_X
-cvs co -r V00-00-09 EgammaAnalysis/ElectronTools
-cvs co -r V09-00-01 RecoEgamma/EgammaTools
-cd EgammaAnalysis/ElectronTools/data
+#cvs co -r V00-00-09 EgammaAnalysis/ElectronTools
+#cvs co -r V09-00-01 RecoEgamma/EgammaTools
+#cd EgammaAnalysis/ElectronTools/data
+#cat download.url | xargs wget
+#electron ID
+git cms-addpkg EgammaAnalysis/ElectronTools
+echo "getting electron MVAs "
+cd EgammaAnalysis/ElectronTools/data/
 cat download.url | xargs wget
+cd $CMSSW_BASE/..
 
 cd $CMSSW_BASE/src
 
 #https://twiki.cern.ch/twiki/bin/viewauth/CMS/PileupJetID
-cvs co -r V00-03-04 -d CMGTools/External UserCode/CMG/CMGTools/External
+#cvs co -r V00-03-04 -d CMGTools/External UserCode/CMG/CMGTools/External
 
-#https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFilters#EB_or_EE_Xtals_with_large_laser
-addpkg RecoMET/METFilters V00-00-13-01
-addpkg RecoMET/METAnalyzers V00-00-08
+##https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFilters#EB_or_EE_Xtals_with_large_laser
+#addpkg RecoMET/METFilters V00-00-13-01
+#addpkg RecoMET/METAnalyzers V00-00-08
 
 cd $CMSSW_BASE/..
-git checkout CMSSW_5_3_11
+#git checkout CMSSW
 
 #Install external
-mkdir -p $STPOL_DIR/local/lib
-mkdir -p $STPOL_DIR/local/include
-$STPOL_DIR/setup/install_lhapdf.sh
-cd $CMSSW_BASE/src
-scram setup lhapdffull
-cd $STPOL_DIR
-source setenv.sh
-$STPOL_DIR/setup/install_tunfold.sh
-$STPOL_DIR/setup/install_theta.sh
-#$STPOL_DIR/setup/install_exempi.sh
-$STPOL_DIR/setup/install_pylibs.sh
-$STPOL_DIR/setup/install_hdf5.sh
+#mkdir -p $STPOL_DIR/local/lib
+#mkdir -p $STPOL_DIR/local/include
+#$STPOL_DIR/setup/install_lhapdf.sh
+#cd $CMSSW_BASE/src
+#scram setup lhapdffull
+#cd $STPOL_DIR
+#source setenv.sh
+#$STPOL_DIR/setup/install_tunfold.sh
+#$STPOL_DIR/setup/install_theta.sh
+##$STPOL_DIR/setup/install_exempi.sh
+#$STPOL_DIR/setup/install_pylibs.sh
+#$STPOL_DIR/setup/install_hdf5.sh
 
 #Compile
 cd $CMSSW_BASE/src
