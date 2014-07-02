f3fdc4f9aecd99c6a3aba91e5a14ea3031e0f81c
andresti <andres.tiko@cern.ch>
2014-07-02 13:33:55 +0300

    Run scripts for pdf weights

---
+++ src/step2/pdfweights_mstw2008CPdeut_cfg.py
@@ -0,0 +1,54 @@
+#from make_file_list import *
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+import os
+print "LHAPATH=",os.environ['LHAPATH']
+os.environ['LHAPATH'] = '/home/andres/single_top/stpol_current/stpol/src/step2/pdfsets'
+print "LHAPATH=",os.environ['LHAPATH']
+
+process = cms.Process("STPOLPDF")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+#filelist = get_file_list()
+
+options = VarParsing('analysis')
+options.register ('PowhegTopMassFix', False,
+              VarParsing.multiplicity.singleton,
+              VarParsing.varType.bool,
+              "Apply Powheg scale fix?")
+options.parseArguments()
+
+#print "option", options
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        #filelist
+        #'file:/hdfs/cms/store/user/joosep/T_t-channel_TuneZ2star_8TeV-powheg-tauola/Jul8_51f69b/e6cee8f160cb239f414a74aa40d88ea9/output_noSkim_1_1_Nq6.root'
+    )
+)
+
+process.pdfWeights = cms.EDProducer('PDFWeightProducer',
+    PDFSets = cms.vstring([
+       'MSTW2008CPdeutnlo68cl.LHgrid'
+    ]),
+    doPowhegTopMassFix=cms.bool(options.PowhegTopMassFix)
+)
+
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('pdfOutput.root'),
+    outputCommands = cms.untracked.vstring([
+        "drop *",
+        "keep *_pdfWeights_*_*"
+        ]
+    )
+)
+
+
+process.p = cms.Path(process.pdfWeights)
+
+process.e = cms.EndPath(process.out)
