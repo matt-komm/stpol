9be03ad860b7904cde4d3089c4250f12e82cea8e
Joosep Pata <joosep.pata@cern.ch>
2014-07-10 11:01:37 +0300

    report creation/submission status

--- util/createSubAll.sh
+++ util/createSubAll.sh
@@ -10,7 +10,13 @@ do
     if [ ! -d "$WD" ]; then
         echo "Creating $DIR/$CFG"
         crab -cfg $CFG -create >> create.log 2>&1
+        CREATION_CODE=$?
+        echo "creation exit code: "$CREATION_CODE
+        if [ $CREATION_CODE -ne 0 ]; then
+            echo "ERROR creating job "$WD
+        fi
         crab -c $WD -submit 500 >> create.log 2>&1
+        echo "submission exit code: ", $?
     else
         echo "Skipping config $DIR/$CFG, directory $DIR/$WD exists"
     fi
