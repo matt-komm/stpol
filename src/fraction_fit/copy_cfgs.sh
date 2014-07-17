08eb89c9fb5c33e480ac6311d84311503cc5b274
Joosep Pata <joosep.pata@cern.ch>
2014-07-17 18:32:32 +0300

    added script to prepare config files

---
+++ src/fraction_fit/copy_cfgs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+inf=$1
+od=`dirname $inf`
+
+for syst in jes jer tchan_scale ttjets_scale wjets_scale mass met pu btag_bc btag_l lepton_id lepton_iso lepton_trigger matching wjets_shape top_weight
+do
+    for dir in up down
+    do
+        FN=$od/${syst}__${dir}.cfg
+        cp $inf $FN
+        sed -i '' "s/systematic = nominal/systematic = $syst/" $FN
+        sed -i '' "s/direction = none/direction = $dir/" $FN
+        sed -i '' "s/nominal/${syst}__${dir}/" $FN
+    done
+done
