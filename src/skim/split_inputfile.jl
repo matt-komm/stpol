472ba47e6a57e6c1157ed443856eed66fd2bbecc
Joosep Pata <joosep.pata@cern.ch>
2014-07-23 21:44:27 +0300

    less verbose

--- src/skim/split_inputfile.jl
+++ src/skim/split_inputfile.jl
@@ -1,14 +1,18 @@
 include("../analysis/basedir.jl")
 include("xs.jl")
 
+#list of all input files, good if sorted
+#created with `find crabs -name "*.files.txt" >> in`, prefixed with file:/hdfs/cms/
 infile = ARGS[1]
+
+#prefix for running, typically `step3`
 path = ARGS[2]
 
 d = Dict()
 for inf in split(readall(infile))
     st = sample_type(inf)
     p = "$path/$(st[:tag])/$(st[:iso])/$(st[:systematic])/$(st[:sample])"
-    println(p) 
+    #println(p) 
     if !haskey(d, p)
         d[p] = Any[]
     end
