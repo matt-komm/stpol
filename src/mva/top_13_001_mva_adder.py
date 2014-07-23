0efe7eafde3d2e61ba64a94ca620492151c10328
Joosep Pata <joosep.pata@cern.ch>
2014-07-23 21:43:17 +0300

    terminating comma

--- src/mva/top_13_001_mva_adder.py
+++ src/mva/top_13_001_mva_adder.py
@@ -22,7 +22,7 @@ def main():
         "mass_bj":"bjet_mass",
         "eta_lj":"ljet_eta",
         "mass_lj":"ljet_mass",
-        "pt_bj":"bjet_pt"
+        "pt_bj":"bjet_pt",
     }
 
     varmaps[13] = {"mt_mu":"mtw", "mu_pt":"lepton_pt"}
