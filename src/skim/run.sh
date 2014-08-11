d383369be456439c4f6aa34b60ab516e72e67ac1
Joosep Pata <joosep.pata@cern.ch>
2014-08-11 13:46:30 +0300

    added new mva

--- src/skim/run.sh
+++ src/skim/run.sh
@@ -12,7 +12,10 @@ mvapath=$scpath/../mva
 
 ~/.julia/CMSSW/julia $scpath/skim.jl $outfile $FILE_NAMES
 
-source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/adder.py bdt_sig_bg $mvapath/weights/stpol_bdt_sig_bg_lepton_flavour.weights.xml $outfile.root ) 
+source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/adder.py bdt_sig_bg $mvapath/weights/stpol_bdt_sig_bg_lepton_flavour.weights.xml $outfile.root )
+
+source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/adder.py bdt_sig_bg_2 $mvapath/weights/stpol_bdt_sig_bg_2_lepton_flavour.weights.xml $outfile.root ) 
+
 source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/qcd_mva_adder.py $outfile.root )
 #source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/qcd_mva_adder2.py $outfile.root )
 source ~/local-sl6/root/bin/thisroot.sh && ( python $mvapath/top_13_001_mva_adder.py $outfile.root )
