1ebb24a2d726efa3e755c886262a51fc71ce5ccd
Joosep Pata <joosep.pata@cern.ch>
2014-09-07 21:47:37 +0300

    c parameter with nu

--- src/mva/top_13_001_mva_adder.py
+++ src/mva/top_13_001_mva_adder.py
@@ -23,6 +23,7 @@ def main():
         "eta_lj":"ljet_eta",
         "mass_lj":"ljet_mass",
         "pt_bj":"bjet_pt",
+        "C":"C_with_nu",
     }
 
     varmaps[13] = {"mt_mu":"mtw", "mu_pt":"lepton_pt"}
