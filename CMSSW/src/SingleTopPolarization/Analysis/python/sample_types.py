fdbf661cbf0dc625b9d7f26ffb8fef715b9da12f
Joosep Pata <joosep.pata@cern.ch>
2014-11-03 17:21:21 +0200

    fixed indendation

--- CMSSW/src/SingleTopPolarization/Analysis/python/sample_types.py
+++ CMSSW/src/SingleTopPolarization/Analysis/python/sample_types.py
@@ -51,8 +51,7 @@ def is_signal(sample_name):
         sample_name.lower().startswith("t_t_toleptons") or
         sample_name.lower().startswith("tbar_t_toleptons") or
         sample_name.lower().startswith("t_t") or
-        sample_name.lower().startswith("tbar_t")) 
-        and not
+        sample_name.lower().startswith("tbar_t")) and not
         (sample_name.lower().startswith("tbar_tw") or
         sample_name.lower().startswith("t_tw")))
 
