2b99c0d8858c07ab4f0c52f4365dddf70e561742
andresti <andres.tiko@cern.ch>
2014-12-02 14:31:22 +0200

    Updated QCD scale factors

--- qcd_estimation/fitted_scale_factors.py
+++ qcd_estimation/fitted_scale_factors.py
@@ -1,12 +1,12 @@
 qcd_scale_factors = {}
 qcd_scale_factors["mu"] = {}
 qcd_scale_factors["ele"] = {}
-qcd_scale_factors["mu"]["2j1t"] = 6.771
-qcd_scale_factors["mu"]["2j0t"] = 10.925
-qcd_scale_factors["mu"]["3j1t"] = 0.241
-qcd_scale_factors["mu"]["3j2t"] = 0.068
-qcd_scale_factors["ele"]["2j1t"] = 31.877
-qcd_scale_factors["ele"]["2j0t"] = 47.429
-qcd_scale_factors["ele"]["3j1t"] = 0.329
-qcd_scale_factors["ele"]["3j2t"] = 0.217
+qcd_scale_factors["mu"]["2j1t"] = 6.330
+qcd_scale_factors["mu"]["2j0t"] = 10.666
+qcd_scale_factors["mu"]["3j1t"] = 0.253
+qcd_scale_factors["mu"]["3j2t"] = 0.050
+qcd_scale_factors["ele"]["2j1t"] = 29.824
+qcd_scale_factors["ele"]["2j0t"] = 45.707
+qcd_scale_factors["ele"]["3j1t"] = 0.296
+qcd_scale_factors["ele"]["3j2t"] = 0.193
 
