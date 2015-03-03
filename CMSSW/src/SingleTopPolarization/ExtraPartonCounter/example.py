969de1238b28d23da7800f40de7b7d43a00af86e
matt-komm <mkomm@cern.ch>
2015-03-03 16:51:08 +0100

    added new plugins

---
+++ CMSSW/src/SingleTopPolarization/ExtraPartonCounter/example.py
@@ -0,0 +1,79 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('USER')
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(20)
+)
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
+'''
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/generator/Summer12/WJetsToLNu_scaledown_8TeV-madgraph-tauola/GEN/START52_V9-v2/0001/020F0C81-B5A5-E111-A51A-0025901D625A.root'
+    )
+)
+'''
+
+process.source = cms.Source("PoolSource",
+    #fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v2/00000/000C5D15-AB1A-E211-8BDE-00215E22053A.root'),
+    #fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Summer12_DR53X/TTJets_SemiLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/00144C5F-1114-E211-BCF9-001E67397CC9.root'),
+    fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/001C868B-B2E1-E111-9BE3-003048D4DCD8.root'),
+)
+
+# Output definition
+
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.prunedGenParticles = cms.EDProducer("GenParticlePruner",
+    src=cms.InputTag("genParticles"),
+    select=cms.vstring(
+        "drop  *",
+        "keep status = 3", #keeps all particles from the hard matrix element
+        "+keep abs(pdgId) = 15 & status = 1" #keeps intermediate decaying tau
+        )
+    )
+    
+
+process.pat2pxlio=cms.EDAnalyzer('EDM2PXLIO',
+    SelectEventsFromProcess=cms.vstring("USER"),  
+    SelectEventsFromPath = cms.vstring("generation_step"),
+    OutFileName=cms.untracked.string("wjets.pxlio"),
+    process=cms.untracked.string("test"),
+    
+    genCollection = cms.PSet(
+        type=cms.string("GenParticle2Pxlio"),
+        srcs=cms.VInputTag(cms.InputTag("prunedGenParticles")),
+        EventInfo=cms.InputTag('generator')
+    ),
+    
+    genJets = cms.PSet(
+        type=cms.string("GenJet2Pxlio"),
+        srcs=cms.VInputTag("ak5GenJets","kt4GenJets","kt6GenJets"),
+        names=cms.vstring("AK5GenJets","KT4GenJets","KT6GenJets")
+    ),
+    
+    q2weights = cms.PSet(
+        type=cms.string("ValueList2Pxlio"),
+        srcs=cms.VInputTag(
+            cms.InputTag("q2weights","weights"),
+            cms.InputTag("q2weights","pdf"),
+            cms.InputTag("q2weights","alphas"),
+            cms.InputTag("q2weights","qcdvertices")
+        ),
+        names = cms.vstring("q2_weights","q2_pdf","q2_alphas","q2_qcdvertices")
+    )
+    
+    
+)
+
+
+process.extraPartons = cms.EDProducer('ExtraPartonCounter'
+)
+
+
+process.out = cms.OutputModule("PoolOutputModule",
+        fileName = cms.untracked.string("test.root")
+)
+
+
+process.p0 = cms.Path(process.prunedGenParticles*process.extraPartons)
+process.pxlioOut=cms.EndPath(process.out*process.pat2pxlio)
+
---
+++ CMSSW/src/SingleTopPolarization/ExtraPartonCounter/plugins/ExtraPartonCounter.cc
@@ -0,0 +1,159 @@
+// system include files
+#include <memory>
+
+// user include files
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+//
+// class declaration
+//
+
+class ExtraPartonCounter : public edm::EDProducer{
+   public:
+      explicit ExtraPartonCounter(const edm::ParameterSet&);
+      ~ExtraPartonCounter();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+
+
+      virtual void beginJob() ;
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+      virtual void endRun(edm::Run const&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+
+ };
+
+//
+// constructors and destructor
+//
+ExtraPartonCounter::ExtraPartonCounter(const edm::ParameterSet& iConfig)
+{
+  produces<std::vector<double> >("qcdvertices");
+}
+
+
+ExtraPartonCounter::~ExtraPartonCounter()
+{
+ 
+ 
+}
+
+void
+ExtraPartonCounter::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+  using namespace std;
+  using namespace reco;
+  
+  
+  
+  std::auto_ptr<std::vector<double>>  nExtraParton(new std::vector<double>(1));
+  
+ 
+  
+  edm::Handle<GenParticleCollection> genPart;
+  iEvent.getByLabel("genParticles", genPart);
+  
+
+    for(size_t i = 0; i < genPart->size(); ++ i) 
+    {
+    
+        const GenParticle & p = (*genPart)[i];
+        if ( p.status() == 3 && (fabs(p.pdgId())<6 || (p.pdgId())==21)) 
+        { 
+            bool is_final= true;
+            bool is_extra = true;
+            for(size_t j=0; j < p.numberOfDaughters(); j++)
+            {
+                 if(p.daughter(j)->status()==3)
+                 {
+                    is_final=false;
+                    break;
+                 }
+            }
+            for(size_t j=0; j < p.numberOfMothers(); j++)
+            {
+                if ((fabs(p.mother(j)->pdgId())==6) ||  (fabs(p.mother(j)->pdgId())==24))
+                {
+                    is_extra=false;
+                    break;
+                }
+            }
+            
+            if (is_final && is_extra)
+            {
+                ++(*nExtraParton)[0];
+            }
+        }
+    }
+  iEvent.put(nExtraParton, "nExtraParton");
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+ExtraPartonCounter::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+ExtraPartonCounter::endJob() 
+{
+}
+
+// ------------ method called when starting to processes a run  ------------
+void 
+ExtraPartonCounter::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a run  ------------
+void 
+ExtraPartonCounter::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when starting to processes a luminosity block  ------------
+void 
+ExtraPartonCounter::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void 
+ExtraPartonCounter::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+ExtraPartonCounter::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ExtraPartonCounter);
